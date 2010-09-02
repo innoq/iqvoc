70011c60596cd79e669d1d8291b30c7564523dc5
Robert Glaser <mail@robert-glaser.de>
2010-09-02 14:41:36 +0200

    Restructured routes, DRYed up named routes within a language scope block.

--- config/routes.rb
+++ config/routes.rb
@@ -1,42 +1,17 @@
 Iqvoc::Application.routes.draw do
   available_locales = /de|en/
-  
-  match ':lang/labels/:origin/versions/branch'    => 'label_versions#branch',    :as => 'label_versions_branch'
-  match ':lang/labels/:origin/versions/merge'     => 'label_versions#merge',     :as => 'label_versions_merge'
-  match ':lang/labels/:origin/versions/lock'      => 'label_versions#lock',      :as => 'label_versions_lock'
-  match ':lang/labels/:origin/versions/unlock'    => 'label_versions#unlock',    :as => 'label_versions_unlock'
-  match ':lang/labels/:origin/versions/to_review' => 'label_versions#to_review', :as => 'label_versions_to_review'
-  match ':lang/labels/:origin/versions/consistency_check' => 'label_versions#consistency_check', :as => 'label_consistency_check'
 
-  match ':lang/concepts/:origin/versions/branch'    => 'concept_versions#branch',    :as => 'concept_versions_branch'
-  match ':lang/concepts/:origin/versions/merge'     => 'concept_versions#merge',     :as => 'concept_versions_merge'
-  match ':lang/concepts/:origin/versions/lock'      => 'concept_versions#lock',      :as => 'concept_versions_lock'
-  match ':lang/concepts/:origin/versions/unlock'    => 'concept_versions#unlock',    :as => 'concept_versions_unlock'
-  match ':lang/concepts/:origin/versions/consistency_check' => 'concept_versions#consistency_check', :as => 'concept_consistency_check'
-  match ':lang/concepts/:origin/versions/to_review' => 'concept_versions#to_review', :as => 'concept_versions_to_review'
-
-  match ':lang/concepts/alphabetical/:letter'   => 'alphabetical_concepts#index', :as => 'alphabetical_concepts', :via => :get
-  match ':lang/concepts/hierarchical(.:format)' => 'hierarchical_concepts#index', :as => 'hierarchical_concepts', :via => :get
-  match ':lang/concepts/hierarchical_broader(.:format)' => 'hierarchical_broader_concepts#index', 
-    :as  => 'hierarchical_broader_concepts',
-    :via => :get
-    
-  match ':lang/search'    => 'search_results#index', :as => 'search'
-  match ':lang/about'     => 'pages#about',          :as => 'about'
-  match ':lang/dashboard' => 'dashboard#index',      :as => 'dashboard'
-  
-  match 'suggest/concepts.:format' => 'concepts#index', :as => 'concept_suggestion'
-  match 'suggest/labels.:format'   => 'labels#index',   :as => 'label_suggestion'
-  
+  # Language parameter is optional; rdf uris don't need to be localized in a any way.
   scope '(:lang)', :lang => available_locales do
     resources :concepts
     resources :labels
   end
   
+  # Language paramater is mandatory.
   scope ':lang' do
     resource  :user_session
     resources :virtuoso_syncs, :only => [:new, :create]
-    
+
     resources :versioned_concepts, :except => :index do
       resources :pref_labelings
       resources :alt_labelings
@@ -44,7 +19,7 @@ Iqvoc::Application.routes.draw do
       resources :narrowers
       resources :related
     end
-    
+
     resources :versioned_labels, :except => :index do
       resources :homographs
       resources :qualifiers
@@ -53,26 +28,49 @@ Iqvoc::Application.routes.draw do
         resources :compound_form_contents
       end
     end
-    
+
     resources :labelings
     resources :users
     resources :notes
     resources :label_relations
     resources :inflectionals, :only => [:index]
+
+    match 'labels/:origin/versions/branch'    => 'label_versions#branch',    :as => 'label_versions_branch'
+    match 'labels/:origin/versions/merge'     => 'label_versions#merge',     :as => 'label_versions_merge'
+    match 'labels/:origin/versions/lock'      => 'label_versions#lock',      :as => 'label_versions_lock'
+    match 'labels/:origin/versions/unlock'    => 'label_versions#unlock',    :as => 'label_versions_unlock'
+    match 'labels/:origin/versions/to_review' => 'label_versions#to_review', :as => 'label_versions_to_review'
+    match 'labels/:origin/versions/consistency_check' => 'label_versions#consistency_check', :as => 'label_consistency_check'
+
+    match 'concepts/:origin/versions/branch'    => 'concept_versions#branch',    :as => 'concept_versions_branch'
+    match 'concepts/:origin/versions/merge'     => 'concept_versions#merge',     :as => 'concept_versions_merge'
+    match 'concepts/:origin/versions/lock'      => 'concept_versions#lock',      :as => 'concept_versions_lock'
+    match 'concepts/:origin/versions/unlock'    => 'concept_versions#unlock',    :as => 'concept_versions_unlock'
+    match 'concepts/:origin/versions/consistency_check' => 'concept_versions#consistency_check', :as => 'concept_consistency_check'
+    match 'concepts/:origin/versions/to_review' => 'concept_versions#to_review', :as => 'concept_versions_to_review'
+
+    match 'concepts/alphabetical/:letter'   => 'alphabetical_concepts#index', :as => 'alphabetical_concepts'
+    match 'concepts/hierarchical(.:format)' => 'hierarchical_concepts#index', :as => 'hierarchical_concepts'
+    match 'concepts/hierarchical_broader(.:format)' => 'hierarchical_broader_concepts#index', :as  => 'hierarchical_broader_concepts'
+
+    match 'search'    => 'search_results#index', :as => 'search'
+    match 'about'     => 'pages#about',          :as => 'about'
+    match 'dashboard' => 'dashboard#index',      :as => 'dashboard'
   end
+  
+  match 'suggest/concepts.:format' => 'concepts#index', :as => 'concept_suggestion'
+  match 'suggest/labels.:format'   => 'labels#index',   :as => 'label_suggestion'
 
   resources :sns_services do
     collection do
       get :get_synonyms
     end
   end
-  
+
   match '/:lang' => 'hierarchical_concepts#index', :lang => available_locales, :as => 'localized_root'
 
-  # map.localized_root ':lang', :controller => 'hierarchical_concepts'
-  # map.localized_root ':lang', :controller => 'hierarchical_broader_concepts'
   root :to => redirect("/de")
-  
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
