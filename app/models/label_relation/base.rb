6f6645a037a537937e6fb11f0c8f5ba7ce2acf5b
Robert Glaser <mail@robert-glaser.de>
2010-09-13 11:27:51 +0200

    Moved label_relation to its own namespace.

---
+++ app/models/label_relation/base.rb
@@ -0,0 +1,22 @@
+class LabelRelation < ActiveRecord::Base
+  
+  belongs_to :domain, :class_name => 'Label::SKOSXL::Base'
+  belongs_to :range,  :class_name => 'Label::SKOSXL::Base'
+
+   scope :published, lambda { |domain_id| {
+    :joins => :range,
+    :conditions => ["(label_relations.domain_id = #{domain_id} AND labels.published_at IS NOT NULL) OR (label_relations.domain_id = #{domain_id} AND labels.rev = 1 AND labels.published_at IS NULL)"] }
+  }
+
+  scope :published_without_initial_versions, lambda { |domain_id| {
+    :joins => :range,
+    :conditions => ["(label_relations.domain_id = #{domain_id} AND labels.published_at IS NOT NULL)"] }
+  }
+
+  scope :range_in_edit_mode, lambda {|domain_id| { 
+    :joins => :range,
+    :include => :range,
+    :conditions => "(label_relations.domain_id = #{domain_id}) AND (labels.locked_by IS NOT NULL)" }
+   }
+
+end
