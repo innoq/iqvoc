58bfdf2194ff7a7d8e82f5ea72b663fe2fd787b7
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2010-09-14 10:42:33 +0200

    Added some "self" confidence to iqvoc.rb

--- lib/iqvoc.rb
+++ lib/iqvoc.rb
@@ -3,26 +3,32 @@ module Iqvoc
   module Concept
     mattr_accessor :base_class_name, :note_class_names, :relation_class_names, :labeling_class_names
     
-    def base_class
+    self.base_class_name        = 'Concept::SKOS::Base'
+    self.relation_class_names   = [ 'Concept::Relation::SKOS::Broader::Poly', 'Concept::Relation::Narrower', 'Concept::Relation::Related' ]
+    self.note_class_names       = [ 'Note::SKOS::ChangeNote', 'Note::SKOS::Definition' ]
+    self.labeling_class_names   = { 'Labeling::SKOSXL::PrefLabel' => [:de, :en], 'Labeling::SKOSXL::AltLabel' => [:de, :en] }
+
+    def self.base_class
       base_class_name.constantize
     end
-    
-    base_class_name        = 'Concept::SKOS::Base'
-    relation_class_names   = [ 'Concept::Relation::SKOS::Broader::Poly', 'Concept::Relation::Narrower', 'Concept::Relation::Related' ]
-    note_class_names       = [ 'Note::SKOS::ChangeNote', 'Note::SKOS::Definition' ]
-    labeling_class_names   = { 'Labeling::SKOSXL::PrefLabel' => [:de, :en], 'Labeling::SKOSXL::AltLabel' => [:de, :en] }
+
   end
   
   module Label
     mattr_accessor :base_class_name, :note_class_names, :relation_class_names
     
-    base_class_name       = 'Label::SKOSXL::Base'
-    relation_class_names  = []
-    note_class_names      = [ 'Note::SKOS::Definition', 
-                              'Note::SKOS::HistoryNote', 
-                              'Note::SKOS::ScopeNote',
-                              'Note::SKOS::EditorialNote',
-                              'Note::SKOS::Example' ]
+    self.base_class_name       = 'Label::SKOSXL::Base'
+    self.relation_class_names  = []
+    self.note_class_names      = [ 'Note::SKOS::Definition',
+      'Note::SKOS::HistoryNote',
+      'Note::SKOS::ScopeNote',
+      'Note::SKOS::EditorialNote',
+      'Note::SKOS::Example' ]
+
+    def self.base_class
+      base_class_name.constantize
+    end
+
   end
   
 end
