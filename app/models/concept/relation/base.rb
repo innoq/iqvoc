26374f1d44c662c4743195e5b37df0ae451e59fe
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2010-09-14 14:09:50 +0200

    Added a method for relation_name guessing

--- app/models/concept/relation/base.rb
+++ app/models/concept/relation/base.rb
@@ -10,4 +10,11 @@ class Concept::Relation::Base < ActiveRecord::Base
   scope :initial_version, joins(:target) & Iqvoc::Concept.base_class.initial_version
   scope :target_in_edit_mode, joins(:target) & Iqvoc::Concept.base_class.in_edit_mode
   
+  # Returnes a name for a relation holding Relation-Objects of a specific class.
+  # 
+  # Concept::Relation::SKOS::Narrower.relation_name # => "concept_relation_skos_narrowers"
+  def self.relation_name
+    name.underscore.pluralize
+  end
+
 end
