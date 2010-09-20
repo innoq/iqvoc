8afc9c3d5f2695c062345d9877ac26437b514e87
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2010-09-20 12:15:06 +0200

    Temporarily removed a scope not working coorrectly

--- app/models/concept/relation/base.rb
+++ app/models/concept/relation/base.rb
@@ -7,7 +7,7 @@ class Concept::Relation::Base < ActiveRecord::Base
 
   scope :by_owner, lambda { |owner_id| where(:owner_id => owner_id) }
   scope :published, joins(:target) & Concept::Base.published
-  scope :initial_version, joins(:target) & Concept::Base.initial_version
+ # scope :initial_version, joins(:target) & Concept::Base.initial_version # FIXME: Won't work because initial_version takes an agrument
   scope :target_in_edit_mode, joins(:target) & Concept::Base.in_edit_mode
 
 end
