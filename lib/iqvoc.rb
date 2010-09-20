c7e664c42bb5beba7f1c74884728cbad980b805b
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2010-09-20 12:16:33 +0200

    Concept view is running again

--- lib/iqvoc.rb
+++ lib/iqvoc.rb
@@ -6,6 +6,7 @@ module Iqvoc
     mattr_accessor :base_class_name, 
       :broader_relation_class_name, :further_relation_class_names,
       :pref_labeling_class_name, :pref_labeling_languages, :further_labeling_class_names,
+      :match_class_names,
       :note_class_names
     
     self.base_class_name              = 'Concept::SKOS::Base'
@@ -19,6 +20,8 @@ module Iqvoc
 
     self.note_class_names             = [ 'Note::SKOS::ChangeNote', 'Note::SKOS::Definition' ]
 
+    self.match_class_names            = [ 'Match::SKOS::Close', 'Match::SKOS::Broader', 'Match::SKOS::Narrower', 'Match::SKOS::Related', 'Match::SKOS::Exact' ]
+
     # Do not use the following method in models. This will propably cause a
     # loading loop (something like "expected file xyz to load ...")
     def self.base_class
@@ -27,6 +30,20 @@ module Iqvoc
 
     def self.pref_labeling_class
       pref_labeling_class_name.constantize
+    end
+
+    def self.further_labeling_classes
+      further_labeling_class_names.keys.each_with_object({}) do |class_name, hash|
+        hash[class_name.constantize] = further_labeling_class_names[class_name]
+      end
+    end
+
+    def self.further_relation_classes
+      further_relation_class_names.map(&:constantize)
+    end
+
+    def self.match_classes
+      match_class_names.map(&:constantize)
     end
 
   end
