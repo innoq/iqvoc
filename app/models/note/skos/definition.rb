4f6591cd6f668a9076db557a4a640a522ddea59e
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2010-09-22 09:47:18 +0200

    Definitions are now showing up in "main" section

--- app/models/note/skos/definition.rb
+++ app/models/note/skos/definition.rb
@@ -1,8 +1,11 @@
 class Note::SKOS::Definition < Note::SKOS::Base
 
-  # FIXME: same as in Note::Base! (deletion?)
-  def to_s
-    value
+  def self.view_section
+    "main"
   end
-  
+
+  def self.view_section_sort_key
+    500 # Show near the end of the section
+  end
+
 end
