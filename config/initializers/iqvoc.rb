ea92a4b011aa79729aaf5a7c4252fc61443228b7
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2010-09-23 10:04:09 +0200

    Made one "versioning" module out of the three common modules

--- config/initializers/iqvoc.rb
+++ config/initializers/iqvoc.rb
@@ -1,8 +1,6 @@
 require 'iqvoc'
 
-require 'iqvoc_global/common_associations'
-require 'iqvoc_global/common_methods'
-require 'iqvoc_global/common_scopes'
+require 'iqvoc_global/versioning'
 require 'iqvoc_global/concept_association_extensions'
 require 'iqvoc_global/deep_cloning'
 require 'iqvoc_global/rdf_helper'
