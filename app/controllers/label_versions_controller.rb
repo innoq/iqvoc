fe94b121196ec8360c806fc834037fe20b87dc7d
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2010-09-28 09:57:40 +0200

    Fishished LabelsVersionsController refactoings

--- app/controllers/label_versions_controller.rb
+++ app/controllers/label_versions_controller.rb
@@ -41,8 +41,8 @@ class LabelVersionsController < ApplicationController
   #Creates a new Version of a Label
   def branch
     current_label = Iqvoc::XLLabel.base_class.by_origin(params[:origin]).published.last
-    raise ActiveRecord::RecordNotFound.new("Couldn't find published label with origin '#{params[:origin]}'") unless current_label
-    raise "There is already an unpublished version for Concept '#{params[:origin]}'" if Iqvoc::XLLabel.base_class.by_origin(params[:origin]).unpublished.last
+    raise ActiveRecord::RecordNotFound.new("Couldn't find published Label with origin '#{params[:origin]}'") unless current_label
+    raise "There is already an unpublished version for Label '#{params[:origin]}'" if Iqvoc::XLLabel.base_class.by_origin(params[:origin]).unpublished.last
     authorize! :branch, current_label
     new_version = nil
     ActiveRecord::Base.transaction do
@@ -53,82 +53,54 @@ class LabelVersionsController < ApplicationController
     redirect_to edit_versioned_label_path(:id => new_version, :lang => @active_language, :check_associations_in_editing_mode => true)
   end
 
-  #Locks the label
   def lock
-    current_version = Iqvoc::XLLabel.base_class.by_origin(params[:origin]).published.last
     new_version = Iqvoc::XLLabel.base_class.by_origin(params[:origin]).unpublished.last
-    if !new_version.blank?
-      if !new_version.locked?
-        new_version.lock_by_user!(current_user.id)
-        if new_version.save
-          flash[:notice] = t("txt.controllers.versioning.locked")
-          redirect_to edit_versioned_label_path(:id => new_version, :lang => @active_language)
-        end
-      else
-        flash[:error] = t("txt.controllers.versioning.lock_error")
-        redirect_to versioned_label_path(:id => new_version, :lang => @active_language)
-      end
+    raise ActiveRecord::RecordNotFound.new("Couldn't find unpublished Label with origin '#{params[:origin]}'") unless new_version
+    raise "Label with origin '#{params[:origin]}' has already been locked." if new_version.locked?
 
-    else
-      flash[:error] = t("txt.controllers.versioning.new_version_blank_error")
-      redirect_to label_path(:lang => @active_language, :id => current_version)
-    end
+    authorize! :lock, new_version
+
+    new_version.lock_by_user!(current_user.id)
+    new_version.save!
+
+    flash[:notice] = t("txt.controllers.versioning.locked")
+    redirect_to edit_versioned_label_path(:id => new_version, :lang => @active_language)
   end
 
   #Unlocks the label
   def unlock
-    current_version = Iqvoc::XLLabel.base_class.current_version(params[:origin]).first
     new_version = Iqvoc::XLLabel.base_class.by_origin(params[:origin]).unpublished.last
-    if !new_version.blank?
-      if new_version.locked?
-        authorize! :unlock, new_version
-        new_version.unlock!
-        if new_version.save
-          flash[:notice] = t("txt.controllers.versioning.unlocked")
-          redirect_to versioned_label_path(:id => new_version, :lang => @active_language)
-        end
-      else
-        flash[:error] = t("txt.controllers.versioning.unlock_error")
-        redirect_to versioned_label_path(:id => new_version, :lang => @active_language)
-      end
+    raise ActiveRecord::RecordNotFound.new("Couldn't find unpublished Label with origin '#{params[:origin]}'") unless new_version
+    raise "Label with origin '#{params[:origin]}' wasn't locked." unless new_version.locked?
 
-    else
-      flash[:error] = t("txt.controllers.versioning.new_version_blank_error")
-      redirect_to label_path(:lang => @active_language, :id => current_version)
-    end
+    authorize! :unlock, new_version
+
+    new_version.unlock!
+    new_version.save!
+
+    flash[:notice] = t("txt.controllers.versioning.unlocked")
+    redirect_to versioned_label_path(:id => new_version, :lang => @active_language)
   end
 
   def consistency_check
-    @label = Iqvoc::XLLabel.base_class.by_origin(params[:origin]).unpublished.last
-    raise ActiveRecord::RecordNotFound unless @label
-    if @label.valid_with_full_validation?
-      if @label.has_concept_or_label_relations?
-        flash[:notice] = t("txt.controllers.versioning.consistency_check_success")
-        redirect_to versioned_label_path(@active_language, @label)
-      else
-        flash[:error] = t("txt.controllers.versioning.consistency_check_success_with_warning")
-        redirect_to versioned_label_path(@active_language, @label)
-      end
+    label = Iqvoc::XLLabel.base_class.by_origin(params[:origin]).unpublished.last
+    raise ActiveRecord::RecordNotFound unless label
+    if label.valid_with_full_validation?
+      flash[:notice] = t("txt.controllers.versioning.consistency_check_success")
+      redirect_to versioned_label_path(:id => label, :lang => @active_language)
     else
-      @concepts_as_pref_label = @label.concepts_as_pref_label.all(:include => :pref_labels)
-      @concepts_as_alt_label = @label.concepts_as_alt_label.all(:include => :pref_labels)
-      @compound_in = Iqvoc::XLLabel.base_class.compound_in(@label).all
       flash[:error] = t("txt.controllers.versioning.consistency_check_error")
-      render :template => "versioned_labels/edit"
+      redirect_to edit_versioned_label_path(:id => label, :lang => @active_language)
     end
   end
 
   def to_review
-    @label = Iqvoc::XLLabel.base_class.by_origin(params[:origin]).unpublished.last
-    raise ActiveRecord::RecordNotFound unless @label
-    @label.to_review!
-    if @label.save
-      flash[:notice] = t("txt.controllers.versioning.to_review_success")
-      redirect_to versioned_label_path(@active_language, @label)
-    else
-      flash[:error] = t("txt.controllers.versioning.to_review_error")
-      redirect_to versioned_label_path(@active_language, @label)
-    end
+    label = Iqvoc::XLLabel.base_class.by_origin(params[:origin]).unpublished.last
+    raise ActiveRecord::RecordNotFound unless label
+    label.to_review!
+    label.save!
+    flash[:notice] = t("txt.controllers.versioning.to_review_success")
+    redirect_to versioned_label_path(@active_language, label)
   end
   
 end
