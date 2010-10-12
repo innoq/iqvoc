71239962996ec3d2a9d19ed4054ff913f689d648
Robert Glaser <mail@robert-glaser.de>
2010-10-12 15:57:55 +0200

    Removed unneeded language_switch_controller.

--- app/controllers/language_switch_controller.rb
+++
@@ -1,8 +0,0 @@
-class LanguageSwitchController < ApplicationController
-  skip_before_filter :require_user
-  
-  def index
-    redirect_to localized_root_path(:lang => I18n.default_locale)
-  end
-  
-end
