94d9c2886a1fab35469cae129bc73254387c3bb0
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2010-10-15 10:52:30 +0200

    I18n didn't work the way I expected... new FIXME... :-(

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Iqvoc::Application.routes.draw do
-  available_locales = /#{I18n.available_locales.join('|')}/
-  
+  available_locales = /#{I18n.available_locales.join('|')}/ # FIXME I18n.available_locales == [:en]???????????????????
+  available_locales = /en|de/ # FIXME :-(
+
   scope ':lang', :lang => available_locales do
     resource  :user_session
     resources :virtuoso_syncs, :only => [:new, :create]
@@ -22,20 +23,20 @@ Iqvoc::Application.routes.draw do
     resources :label_relations
     
     %w(concepts labels).each do |type|
-      match "#{type}/versions/:origin/branch"      => "#{type}/versions#branch",    :as => "#{type.singularize}_versions_branch"
-      match "#{type}/versions/:origin/merge"       => "#{type}/versions#merge",     :as => "#{type.singularize}_versions_merge"
-      match "#{type}/versions/:origin/lock"        => "#{type}/versions#lock",      :as => "#{type.singularize}_versions_lock"
-      match "#{type}/versions/:origin/unlock"      => "#{type}/versions#unlock",    :as => "#{type.singularize}_versions_unlock"
-      match "#{type}/versions/:origin/to_review"   => "#{type}/versions#to_review", :as => "#{type.singularize}_versions_to_review"
-      match "#{type}/versions/:origin/consistency_check" => "#{type}/versions#consistency_check", :as => "#{type.singularize}_versions_consistency_check"
+      match "#{type}/versions/:origin/branch(.:format)"      => "#{type}/versions#branch",    :as => "#{type.singularize}_versions_branch"
+      match "#{type}/versions/:origin/merge(.:format)"       => "#{type}/versions#merge",     :as => "#{type.singularize}_versions_merge"
+      match "#{type}/versions/:origin/lock(.:format)"        => "#{type}/versions#lock",      :as => "#{type.singularize}_versions_lock"
+      match "#{type}/versions/:origin/unlock(.:format)"      => "#{type}/versions#unlock",    :as => "#{type.singularize}_versions_unlock"
+      match "#{type}/versions/:origin/to_review(.:format)"   => "#{type}/versions#to_review", :as => "#{type.singularize}_versions_to_review"
+      match "#{type}/versions/:origin/consistency_check(.:format)" => "#{type}/versions#consistency_check", :as => "#{type.singularize}_versions_consistency_check"
     end
 
-    match 'alphabetical_concepts/:letter'   => 'concepts/alphabetical#index', :as => 'alphabetical_concepts'
+    match 'alphabetical_concepts/:letter(.:format)'   => 'concepts/alphabetical#index', :as => 'alphabetical_concepts'
     match 'hierarchical_concepts(.:format)' => 'concepts/hierarchical#index', :as => 'hierarchical_concepts'
 
-    match 'search'    => 'search_results#index', :as => 'search'
-    match 'about'     => 'pages#about',          :as => 'about'
-    match 'dashboard' => 'dashboard#index',      :as => 'dashboard'
+    match 'search(.:format)'    => 'search_results#index', :as => 'search'
+    match 'about(.:format)'     => 'pages#about',          :as => 'about'
+    match 'dashboard(.:format)' => 'dashboard#index',      :as => 'dashboard'
 
     root :to => 'concepts/hierarchical#index', :as => 'localized_root'
   end
@@ -51,5 +52,5 @@ Iqvoc::Application.routes.draw do
 
   root :to => redirect("/de")
 
-  match '/:id' => 'rdf#show', :as => 'rdf'
+  match '/:id(.:format)' => 'rdf#show', :as => 'rdf'
 end
