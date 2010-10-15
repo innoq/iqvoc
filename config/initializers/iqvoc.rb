42e5f574f491211a77d81ec474bdf99c9d811d36
Robert Glaser <mail@robert-glaser.de>
2010-10-15 13:03:45 +0200

    Moved vendor specific Iqvoc initialization to engine.

--- config/initializers/iqvoc.rb
+++ config/initializers/iqvoc.rb
@@ -5,31 +5,3 @@ require 'iqvoc_global/deep_cloning'
 require 'iqvoc_global/rdf_helper'
 
 ActiveRecord::Base.send :include, IqvocGlobal::DeepCloning
-
-Iqvoc::Concept.note_class_names += [
-  'Note::UMT::ChangeNote',
-  'Note::UMT::ExportNote',
-  'Note::UMT::SourceNote',
-  'Note::UMT::UsageNote'
-]
-
-Iqvoc::XLLabel.base_class_name = 'Label::UMT::Base'
-
-Iqvoc::XLLabel.relation_class_names += [
-  'Label::Relation::UMT::Translation',
-  'Label::Relation::UMT::Homograph',
-  'Label::Relation::UMT::Qualifier',
-  'Label::Relation::UMT::LexicalExtension'
-]
-
-Iqvoc::XLLabel.note_class_names = Iqvoc::Concept.note_class_names
-
-Iqvoc::XLLabel.additional_association_classes.merge(
-  "Inflectional::Base" => "label_id",
-  "CompoundForm::Base" => "domain_id",
-  "CompoundForm::Content::Base" => "label_id" # This is used for the reverse direction ('compound_in')
-)
-
-Iqvoc::XLLabel.view_sections << "compound_forms"
-
-Iqvoc::XLLabel.has_additional_base_data = true
