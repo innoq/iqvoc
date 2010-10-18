6b3fb910715f48e3cc88ff9f0f3cf0a9193614e8
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2010-10-18 14:26:04 +0200

    Made fieldWithError proc rails 3.0 ready

--- config/initializers/action_view.rb
+++ config/initializers/action_view.rb
@@ -1,7 +1,7 @@
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
   if instance.error_message.kind_of?(Array)
-    %(#{html_tag}<span class="fieldWithErrors">&nbsp;</span>)
+    %(#{html_tag}<span class="fieldWithErrors">&nbsp;</span>).html_safe
   else
-    %(#{html_tag}<span class="fieldWithErrors">&nbsp;</span>)
+    %(#{html_tag}<span class="fieldWithErrors">&nbsp;</span>).html_safe
   end
 end
