08dc87dac9b94e19f34ea502ea824eb2c1306d47
Robert Glaser <mail@robert-glaser.de>
2010-10-19 12:59:17 +0200

    Removed *facebook style names.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -73,16 +73,16 @@ jQuery(document).ready(function() {
       }) : null,
             
       classes: {
-        tokenList: "token-input-list-facebook",
-        token: "token-input-token-facebook",
-        tokenDelete: "token-input-delete-token-facebook",
-        selectedToken: "token-input-selected-token-facebook",
-        highlightedToken: "token-input-highlighted-token-facebook",
-        dropdown: "token-input-dropdown-facebook",
-        dropdownItem: "token-input-dropdown-item-facebook",
-        dropdownItem2: "token-input-dropdown-item2-facebook",
-        selectedDropdownItem: "token-input-selected-dropdown-item-facebook",
-        inputToken: "token-input-input-token-facebook"
+        tokenList: "token-input-list-iqvoc",
+        token: "token-input-token-iqvoc",
+        tokenDelete: "token-input-delete-token-iqvoc",
+        selectedToken: "token-input-selected-token-iqvoc",
+        highlightedToken: "token-input-highlighted-token-iqvoc",
+        dropdown: "token-input-dropdown-iqvoc",
+        dropdownItem: "token-input-dropdown-item-iqvoc",
+        dropdownItem2: "token-input-dropdown-item2-iqvoc",
+        selectedDropdownItem: "token-input-selected-dropdown-item-iqvoc",
+        inputToken: "token-input-input-token-iqvoc"
       }
     });
   });
