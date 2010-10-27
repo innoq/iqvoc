cdf993ed5d8e47769911df4db3df54bdd2f92d59
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2010-10-27 14:39:33 +0200

    Searchability for Label::SKOS::Base

--- app/models/label/skos/base.rb
+++ app/models/label/skos/base.rb
@@ -6,4 +6,19 @@ class Label::SKOS::Base < Label::Base
     self.published_at = Time.now
   end
 
+  # ********** Methods
+
+  def self.single_query(params = {})
+    query_str = build_query_string(params)
+
+    by_query_value(query_str).
+      by_language(params[:languages].to_a).
+      published.
+      order("LOWER(#{Label::Base.arel_table[:value].to_sql})")
+  end
+
+  def self.search_result_partial_name
+    'partials/label/skos/search_result'
+  end
+
 end
