18f64ec3353b0541b23d4f185e63c0a0895b3cb7
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2010-11-08 13:41:10 +0100

    Split capistrano deployment in multiple files

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,6 +20,16 @@ require 'bundler/capistrano'
 # main details
 set :application, "iqvoc"
 
+# repo details
+set :scm, :git
+set :git_enable_submodules, 1
+# set :scm_username, "passenger"
+set :repository, "git@github.com:innoq/iqvoc.git"
+@capistrano_history['last_branch'] = "master" if @capistrano_history['last_branch'].nil? || @capistrano_history['last_branch'] == ""
+set :branch, Capistrano::CLI.ui.ask("Please enter the branch or tag we should use [#{@capistrano_history['last_branch']}]: ")
+set :branch, @capistrano_history['last_branch'] if fetch(:branch) == ""
+@capistrano_history['last_branch'] = fetch(:branch)
+
 save_history
 
 # tasks
