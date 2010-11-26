a88cd22212266b12e57290c155d29d9adecb4b7e
Robert Glaser <mail@robert-glaser.de>
2010-11-26 14:27:47 +0100

    Some more controller logic for Collection.

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -6,7 +6,20 @@ class CollectionsController < ApplicationController
   
   def new
     @collection = Collection::SKOS::Base.new
-    @collection.language_notes.build if @collection.language_notes.empty?
+    @collection.note_iqvoc_language_notes.build if @collection.note_iqvoc_language_notes.empty?
+    @collection.note_skos_definitions.build if @collection.note_skos_definitions.empty?
+  end
+  
+  def create
+    @collection = Collection::SKOS::Base.new(params[:collection])
+    
+    if @collection.save
+      flash[:error] = I18n.t("txt.controllers.collections.save.success")
+      redirect_to collections_path(:lang => I18n.locale)
+    else
+      flash[:error] = I18n.t("txt.controllers.collections.save.error")
+      render :new
+    end
   end
   
 end
