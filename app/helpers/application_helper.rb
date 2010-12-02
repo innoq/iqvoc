6fe330416cb9ae333b10158035d227c81f5533d0
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2010-12-02 18:00:38 +0100

    Moved default rdf namespace definition to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,7 @@ module ApplicationHelper
 
   def iqvoc_default_rdf_namespaces
     {
+      :default => root_url(:format => nil, :lang => nil, :trailing_slash => true).gsub(/\/\/$/, "/"), # gsub because of a Rails bug :-(
       :rdf        => "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
       :rdfs       => "http://www.w3.org/2000/01/rdf-schema#",
       :owl        => "http://www.w3.org/2002/07/owl#",
