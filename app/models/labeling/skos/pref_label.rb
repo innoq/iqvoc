461dae469630468946b8668da8c439d29a26adf7
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2010-12-05 19:42:03 +0100

    CamelCased properties

--- app/models/labeling/skos/pref_label.rb
+++ app/models/labeling/skos/pref_label.rb
@@ -1,7 +1,7 @@
 class Labeling::SKOS::PrefLabel < Labeling::SKOS::Base
 
   def build_rdf(document, subject)
-    subject.Skos.pref_label(target.to_s, :lang => target.language)
+    subject.Skos.prefLabel(target.to_s, :lang => target.language)
   end
 
   def self.only_one_allowed?
--- app/models/labeling/skosxl/pref_label.rb
+++ app/models/labeling/skosxl/pref_label.rb
@@ -1,8 +1,8 @@
 class Labeling::SKOSXL::PrefLabel < Labeling::SKOSXL::Base
 
   def build_rdf(document, subject)
-    subject.Skosxl::pref_label(IqRdf.build_uri(target.origin))
-    subject.Skos.pref_label(target.to_s, :lang => target.language)
+    subject.Skosxl::prefLabel(IqRdf.build_uri(target.origin))
+    subject.Skos.prefLabel(target.to_s, :lang => target.language)
   end
 
   def self.only_one_allowed?
