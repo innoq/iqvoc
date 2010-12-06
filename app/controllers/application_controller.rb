3e34988af371be9b00d114e76a06f09e28632ca0
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2010-12-06 16:32:46 +0100

    Fixed search result

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,8 +37,10 @@ class ApplicationController < ActionController::Base
   def handle_not_found(exception)
     @exception = exception
     @available_languages = (Iqvoc.available_languages + Iqvoc::Concept.labeling_class_names.values.flatten).uniq.each_with_object({}) do |lang_sym, hsh|
+      lang_sym ||= "none"
       hsh[lang_sym.to_s] = t("languages.#{lang_sym.to_s}", :default => lang_sym.to_s)
     end
+
     render :template => 'errors/not_found', :status => :not_found
   end
   
