e5975a8a68bb8db887719324250dd1796e863bd5
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2010-12-08 10:21:37 +0100

    Added some more intelligence to the factory

--- features/support/factories.rb
+++ features/support/factories.rb
@@ -1,18 +1,30 @@
-Factory.define :concept, :class => Concept::Base do |c|
+Factory.define :concept, :class => Iqvoc::Concept.base_class do |c|
   c.sequence(:origin) { |n| "_000000#{n}" }
   c.published_at 3.days.ago
+  c.labelings { |labelings| [labelings.association(:pref_labeling)] }
 end
 
 Factory.define :concept_with_associations, :parent => :concept do |c|
+
 end
 
-Factory.define :label, :class => Label::SKOSXL::Base do |l|
+Factory.define :pref_labeling, :class => Iqvoc::Concept.pref_labeling_class do |lab|
+  lab.target { |target| target.association(:pref_label) }
+end
+
+Factory.define :pref_label, :class => Iqvoc::Concept.pref_labeling_class.label_class do |l|
+  l.language Iqvoc::Concept.pref_labeling_languages.first
+  l.value 'Tree'
+  l.origin 'Tree'
+end
+
+Factory.define :xllabel, :class => Iqvoc::XLLabel.base_class do |l|
   l.origin 'Forest'
   l.language 'en'
   l.value 'Forest'
 end
 
-Factory.define :label_with_association, :parent => :label do |l|
+Factory.define :xllabel_with_association, :parent => :xllabel do |l|
 end
 
 Factory.define :user do |u|
