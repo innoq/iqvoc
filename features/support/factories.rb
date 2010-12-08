6afa7cf2e6ffc71a1e5c133329a06f18b6521c1f
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2010-12-08 11:27:08 +0100

    Added relations to :concept factory. Was a little bit tricky...

--- features/support/factories.rb
+++ features/support/factories.rb
@@ -2,10 +2,26 @@ Factory.define :concept, :class => Iqvoc::Concept.base_class do |c|
   c.sequence(:origin) { |n| "_000000#{n}" }
   c.published_at 3.days.ago
   c.labelings { |labelings| [labelings.association(:pref_labeling)] }
+  c.broader_relations { |broader_relations| [broader_relations.association(:broader_relation)]}
+  c.narrower_relations{ |narrower_relations| [narrower_relations.association(:narrower_relation)]}
 end
 
-Factory.define :concept_with_associations, :parent => :concept do |c|
+Factory.define :broader_relation, :class => Iqvoc::Concept.broader_relation_class do |br|
+  br.target { |target|
+    target.association(:concept, :broader_relations => [], :narrower_relations => [], :labelings => [
+        Factory.create(:pref_labeling, :target => Factory.create(:pref_label, :value => 'Some broader relation'))
+      ])
+  }
+  rel.after_create { |new_relation| Factory(:narrower_relation, :owner => new_relation.target, :target => new_relation.owner) }
+end
 
+Factory.define :narrower_relation, :class => Iqvoc::Concept.broader_relation_class.narrower_class do |rel|
+  rel.target {|target|
+    target.association(:concept, :broader_relations => [], :narrower_relations => [], :labelings => [
+        Factory.create(:pref_labeling, :target => Factory.create(:pref_label, :value => 'Some narrower relation'))
+      ])
+  }
+  rel.after_create { |new_relation| Factory(:broader_relation, :owner => new_relation.target, :target => new_relation.owner) }
 end
 
 Factory.define :pref_labeling, :class => Iqvoc::Concept.pref_labeling_class do |lab|
