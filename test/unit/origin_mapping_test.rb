2306a8c63e6f9890addcb4f1528a068a6d5d89d9
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2010-12-08 09:56:26 +0100

    Unit tests (3! :-) ) green again

--- test/unit/origin_mapping_test.rb
+++ test/unit/origin_mapping_test.rb
@@ -3,19 +3,19 @@ require 'test_helper'
 class OriginMappingTest < ActiveSupport::TestCase
 
   def test_should_replace_umlauts
-    assert_equal "AeaeUeueOeoess", OriginMapping.replace_umlauts("ÄäÜüÖöß")
+    assert_equal "AeaeUeueOeoess", OriginMapping.merge("ÄäÜüÖöß")
   end
 
   def test_should_camalize_string
-    assert_equal "'A'Weighting", OriginMapping.to_camelcase("'A' Weighting")
+    assert_equal "'A'Weighting", OriginMapping.merge("'A' Weighting")
   end
 
   def test_should_replace_brackets
-    assert_equal "--Energie - Ressource", OriginMapping.replace_brackets("[Energie - Ressource]")
+    assert_equal "--Energie-Ressource", OriginMapping.merge("[Energie-Ressource]")
   end
 
   def test_should_replace_comma
-    assert_equal "-", OriginMapping.replace_commas(",")
+    assert_equal "-", OriginMapping.merge(",")
   end
 
   def test_should_merge_all_together
