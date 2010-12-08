f521ce4682ac50d95fdaf972da45ba95107b32fc
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2010-12-08 09:46:54 +0100

    "lambdaing" another scope... prevented tests to load...

--- app/models/labeling/base.rb
+++ app/models/labeling/base.rb
@@ -17,8 +17,13 @@ class Labeling::Base < ActiveRecord::Base
     where(:target_id => label.id)
   }
 
-  scope :concept_published, includes(:owner) & Concept::Base.published
-  scope :label_published, includes(:target) & Label::Base.published
+  scope :concept_published, lambda {
+    includes(:owner) & Concept::Base.published
+  }
+  
+  scope :label_published, lambda {
+    includes(:target) & Label::Base.published
+  }
 
   scope :label_begins_with, lambda { |letter|
     includes(:target) & Label::Base.begins_with(letter)
