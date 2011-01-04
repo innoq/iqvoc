62ca57d9b2468a86c74a3fe0854a653c9ea40c3c
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2011-01-04 17:05:12 +0100

    Added release script (partially untested)

---
+++ script/release.rb
@@ -0,0 +1,38 @@
+require 'lib/iqvoc/version'
+require 'rubygems'
+require 'fileutils'
+require 'active_support/core_ext'
+
+FileUtils.rm_rf('tmp/release') if File.directory?('tmp/release')
+FileUtils.mkdir 'tmp/release'
+begin
+  FileUtils.cd('tmp/release')  do
+    git_remotes = `git remote show origin`
+    raise "Couldn't read Fetch URL from #{git_remotes}" unless git_remotes =~ /Fetch URL: (.+)$/
+    fetch_url = $1
+
+    raise "Couldn't read remote branches from #{git_remotes}" unless git_remotes =~ /^\s*Remote branches:\n((\s*\w+(\s+\w*)?\n)+)/
+    branches = $1.split(/\n/).map do |line|
+      line.gsub(/^\s*(\w+)(\s+\w*)?$/) { $1 }
+    end
+
+    branch = nil
+    while true
+      STDOUT.print "Enter branch name [#{branches.first}]: "
+      branch = (STDIN.gets.presence || branches.first).gsub(/\n/, "")
+      break if branches.include?(branch)
+      puts "Branch must be one of #{branches.join(', ')}"
+    end
+
+    `git clone -b #{branch} #{fetch_url} checkout`
+
+    FileUtils.cd('checkout') do
+      puts `jruby -S bundle install --deployment --without=development test`
+    end
+
+    `tar -czf iqvoc_#{Iqvoc::VERSION}.tgz checkout`
+
+  end
+ensure
+  # FileUtils.rm_rf('tmp/release')
+end
