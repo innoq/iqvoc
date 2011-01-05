1b51e10ab7eec7c03dfe0c6a8615d428f00b22eb
Robert Glaser <mail@robert-glaser.de>
2011-01-05 17:13:10 +0100

    Renamed indexes to strings <= 30 bytes. Fuck you, Oracle.

--- db/migrate/20101210153916_add_indexes_to_collections.rb
+++ db/migrate/20101210153916_add_indexes_to_collections.rb
@@ -1,7 +1,7 @@
 class AddIndexesToCollections < ActiveRecord::Migration
   def self.up
-    add_index :collections, [:origin, :type]
-    add_index :collection_members, [:collection_id, :target_id, :type]
+    add_index :collections, [:origin, :type], :name => "ix_collections_origin_type"
+    add_index :collection_members, [:collection_id, :target_id, :type], :name => "ix_collections_fk_type"
   end
 
   def self.down
