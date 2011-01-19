ec6463f723c6c107a6b5fa5117e1d5522eab7dd5
Robert Glaser <mail@robert-glaser.de>
2011-01-19 15:14:43 +0100

    Customized warble file and template.

--- config/warble.rb
+++ config/warble.rb
@@ -94,7 +94,7 @@ Warbler::Config.new do |config|
 
   # Name of the war file (without the .war) -- defaults to the basename
   # of RAILS_ROOT
-  config.war_name = "ROOT"
+  # config.war_name = "mywar"
 
   # Name of the MANIFEST.MF template for the war file. Defaults to a simple
   # MANIFEST.MF that contains the version of Warbler used to create the war file.
@@ -106,7 +106,7 @@ Warbler::Config.new do |config|
   # config.compiled_ruby_files = FileList['app/**/*.rb']
 
   # Value of RAILS_ENV for the webapp -- default as shown below
-  # config.webxml.rails.env = ENV['RAILS_ENV'] || 'production'
+  config.webxml.rails.env = ENV['RAILS_ENV'] || 'production'
 
   # Application booter to use, one of :rack, :rails, or :merb (autodetected by default)
   # config.webxml.booter = :rails
