4db2bcc74036f59b6296bcd34826d6f587fe5edd
Robert Glaser <mail@robert-glaser.de>
2011-01-24 19:02:27 +0100

    Definition needs an own search result partial.

--- app/models/note/skos/definition.rb
+++ app/models/note/skos/definition.rb
@@ -7,5 +7,9 @@ class Note::SKOS::Definition < Note::SKOS::Base
   def self.view_section_sort_key(obj)
     500 # Show near the end of the section
   end
+  
+  def self.search_result_partial_name
+    'partials/note/skos/definition/search_result'
+  end
 
 end
