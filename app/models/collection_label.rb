fb77db1aac3aa6ae0fd1cd978bcaf5040812a11a
Robert Glaser <mail@robert-glaser.de>
2011-02-04 18:44:44 +0100

    Some additions to the new CollectionLabel.

--- app/models/collection_label.rb
+++ app/models/collection_label.rb
@@ -1,6 +1,6 @@
 class CollectionLabel < ActiveRecord::Base
   
-  belongs_to :collection
+  belongs_to :collection, :class_name => 'Collection::Base'
 
   scope :by_language, lambda { |lang_code|
     if (lang_code.is_a?(Array) && lang_code.include?(nil))
@@ -16,6 +16,24 @@ class CollectionLabel < ActiveRecord::Base
     
   def self.edit_partial_name(obj)
     'partials/collection_label/edit'
+  end
+  
+  def self.search_result_partial_name
+    'partials/collection_label/search_result'
+  end
+  
+  def self.single_query(params = {})
+    query_str = build_query_string(params)
+    
+    scope = includes(:collection)
+    
+    if params[:query].present?
+      scope = scope.by_query_value(query_str).by_language(params[:languages].to_a)
+    else
+      scope = scope.by_language(params[:languages].to_a)
+    end
+    
+    scope
   end
   
   def build_rdf(document, subject)
