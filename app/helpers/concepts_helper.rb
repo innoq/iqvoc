2e4f16413e74a0fe6748ebe1318fdddf0f6c982a
FND <fnd@fnd-oda.(none)>
2011-03-01 16:01:43 +0100

    made hierarchical treeview partial accessible
    instead of loading and rendering the entire tree via JavaScript, we
    render root-level items on the server side - due to related concepts
    being linked, this allows user agents to traverse the tree without
    client-side processing
    
    code largely adapted from hierarchical index template (minus broader
    relations); eventually that template should also use this partial (DRY)
    
    behavior seems the same as before, except that order is not necessarily
    the same; unsure why this is the case or whether it is significant
    
    plus minor whitespace fixes

--- app/helpers/concepts_helper.rb
+++ app/helpers/concepts_helper.rb
@@ -2,11 +2,11 @@ module ConceptsHelper
   def select_search_checkbox?(lang)
     (params[:languages] && params[:languages].include?(lang.to_s)) || (!params[:query] && I18n.locale.to_s == lang.to_s)
   end
-  
+
   def quote_turtle_value(str)
     str.match(/^<.*>$/) ? str : "\"#{str}\""
   end
-  
+
   def treeview(root = "source")
     render :partial => "concepts/hierarchical/treeview", :locals => { :root => root }
   end
@@ -45,5 +45,5 @@ module ConceptsHelper
 
     res
   end
-  
+
 end
