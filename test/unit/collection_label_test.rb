9ddc79ef2b42e89ff28cecbd3c2a684f1c72d8dc
FND <fnd@fnd-oda.(none)>
2011-03-31 16:06:35 +0200

    fixed variable name in tests
    convention is to use underscore, not CamelCase

--- test/unit/collection_label_test.rb
+++ test/unit/collection_label_test.rb
@@ -7,7 +7,7 @@ class CollectionLabelTest < ActiveSupport::TestCase
   end
 
   test "can have multiple labels" do
-    myColl = @klass.new
+    my_coll = @klass.new
     # add a few label[ing]s
     origin = 0
     Iqvoc::Concept.labeling_classes.each { |lnclass, langs|
@@ -16,62 +16,62 @@ class CollectionLabelTest < ActiveSupport::TestCase
         label = lnclass.label_class.new(:origin => "_%s" % origin,
             :value => "lipsum_%s" % origin, :language => lang)
         label.save
-        labeling = lnclass.new(:owner => myColl, :target => label)
+        labeling = lnclass.new(:owner => my_coll, :target => label)
         labeling.save
       }
     }
-    myColl.save
+    my_coll.save
 
-    assert myColl.labels.size > 1
-    assert_equal myColl.labels.size, Iqvoc::Concept.labeling_classes.
+    assert my_coll.labels.size > 1
+    assert_equal my_coll.labels.size, Iqvoc::Concept.labeling_classes.
         map { |lnclass, langs| langs }.flatten.size
   end
 
   test "must have a preferred label" do
-    myColl = @klass.new
+    my_coll = @klass.new
 
     # add an alternative label
     lnclass = Labeling::SKOSXL::AltLabel
     label = lnclass.label_class.new(:origin => "_666", :value => "lipsum",
         :language => "de")
     label.save
-    labeling = lnclass.new(:owner => myColl, :target => label)
+    labeling = lnclass.new(:owner => my_coll, :target => label)
     labeling.save
 
     assert_raise ActiveRecord::RecordInvalid do
-      myColl.save_with_full_validation!
+      my_coll.save_with_full_validation!
     end
-    assert_equal 1, myColl.labels.count
+    assert_equal 1, my_coll.labels.count
 
     # add a preferred label
     lnclass = Labeling::SKOSXL::PrefLabel
     label = lnclass.label_class.new(:origin => "_666", :value => "lipsum",
         :language => "de")
     label.save
-    labeling = lnclass.new(:owner => myColl, :target => label)
+    labeling = lnclass.new(:owner => my_coll, :target => label)
     labeling.save
 
-    assert_equal 2, myColl.labels.count
+    assert_equal 2, my_coll.labels.count
   end
 
   test "does not need more than one label" do
-    myColl = @klass.new
+    my_coll = @klass.new
 
     # add a preferred label
     lnclass = Labeling::SKOSXL::PrefLabel
     label = lnclass.label_class.new(:origin => "_666", :value => "lipsum",
         :language => "de")
     label.save
-    labeling = lnclass.new(:owner => myColl, :target => label)
+    labeling = lnclass.new(:owner => my_coll, :target => label)
     labeling.save
 
-    assert_equal true, myColl.save_with_full_validation!
-    assert_equal 1, myColl.pref_labels.count
-    assert_equal 1, myColl.labels.count
+    assert_equal true, my_coll.save_with_full_validation!
+    assert_equal 1, my_coll.pref_labels.count
+    assert_equal 1, my_coll.labels.count
   end
 
   test "can have multiple preferred labels" do
-    myColl = @klass.new
+    my_coll = @klass.new
 
     # add multiple preferred labels
     lnclass = Labeling::SKOSXL::PrefLabel
@@ -81,12 +81,12 @@ class CollectionLabelTest < ActiveSupport::TestCase
       label = lnclass.label_class.new(:origin => "_%s" % origin,
           :value => "lipsum_%s" % origin, :language => "de")
       label.save
-      labeling = lnclass.new(:owner => myColl, :target => label)
+      labeling = lnclass.new(:owner => my_coll, :target => label)
       labeling.save
     }
 
-    assert_equal true, myColl.save_with_full_validation!
-    assert_equal 5, myColl.pref_labels.count
-    assert_equal 5, myColl.labels.count
+    assert_equal true, my_coll.save_with_full_validation!
+    assert_equal 5, my_coll.pref_labels.count
+    assert_equal 5, my_coll.labels.count
   end
 end
