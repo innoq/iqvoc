eb87c2d5dec194a1b30e9fb03735fe5a41636739
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2011-04-06 11:05:54 +0200

    Moved XL Models to iqvoc_skosxl gem

--- app/models/labeling/skosxl/alt_label.rb
+++
@@ -1,8 +0,0 @@
-class Labeling::SKOSXL::AltLabel < Labeling::SKOSXL::Base
-
-  def build_rdf(document, subject)
-    subject.Skosxl::altLabel(IqRdf.build_uri(target.origin))
-    subject.Skos.altLabel(target.to_s, :lang => target.language)
-  end
-
-end
