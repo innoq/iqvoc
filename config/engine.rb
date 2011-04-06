e24bb07bfc9b172b4721247f89d1c81003cdf4db
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2011-04-06 10:14:06 +0200

    Had to require the iqvoc dependencies in the engine. Rails/Bundler doesn't do that for you.

--- config/engine.rb
+++ config/engine.rb
@@ -1,8 +1,22 @@
 require 'rails'
 
+# An engine doesn't require it's own dependencies automatically. We also don't
+# want the applications to have to do that. 
+require 'cancan'
+require 'authlogic'
+require 'will_paginate'
+require 'iq_rdf'
+require 'json'
+
 module Iqvoc
 
   class Engine < Rails::Engine
+
+    # TODO: Is this configuration (s. Application too) still required if iqvoc
+    # runs as engine?
+    config.additional_js_files  = []
+    config.additional_css_files = []
+
   end
 
 end
