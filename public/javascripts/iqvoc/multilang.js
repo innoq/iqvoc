0a6cbff5df5db2a8c5008b0685b6803724b6894a
FND <fnd@fnd-oda.(none)>
2011-04-28 10:29:47 +0200

    removed automatic reset when all languages are de-selected
    this won't be necessary as the main language should not be
    de-selectable, so there's always at least one active language (though
    that's yet to be implemented)
    
    refactored checkbox initialization in the process

--- public/javascripts/iqvoc/multilang.js
+++ public/javascripts/iqvoc/multilang.js
@@ -9,16 +9,24 @@ var getSelection, setSelection;
 var LanguageSelector = function(container, namespace) {
 	this.container = container;
 	this.namespace = namespace;
-
-	$(container).addClass("widget").data("widget", this);
-
 	this.langs = getSelection(namespace);
 	this.checkboxes = $("input:checkbox", container)
 			.live("change", this.onChange);
 
-	if(this.langs.length === 0) {
-		this.reset();
-	} else {
+	$(container).addClass("widget").data("widget", this);
+	this.init();
+};
+$.extend(LanguageSelector.prototype, {
+	onChange: function(ev) {
+		var el = $(this),
+			widget = el.closest(".widget").data("widget");
+		if(el.attr("checked")) {
+			widget.add(el.val());
+		} else {
+			widget.remove(el.val());
+		}
+	},
+	init: function() {
 		var self = this;
 		this.checkboxes.each(function(i, node) {
 			var el = $(node);
@@ -28,40 +36,7 @@ var LanguageSelector = function(container, namespace) {
 				el.removeAttr("checked");
 			}
 		});
-		$(document).trigger(namespace, { langs: this.langs });
-	}
-};
-$.extend(LanguageSelector.prototype, {
-	onChange: function(ev) {
-		var el = $(this);
-		var widget = el.closest(".widget").data("widget");
-		if(el.attr("checked")) {
-			widget.add(el.val());
-		} else {
-			widget.remove(el.val());
-		}
-		if(widget.langs.length === 0) {
-			widget.reset(true);
-		}
-	},
-	reset: function(animate) {
-		var cbs = this.checkboxes;
-		if(animate) {
-			var self = this;
-			var i = cbs.length;
-			cbs.fadeOut().fadeIn(function() {
-				i -= 1;
-				if(i === 0) {
-					self.reset();
-				}
-			});
-		} else {
-			this.langs = $.map(cbs, function(node, i) {
-				return $(node).val();
-			});
-			cbs.attr("checked", "checked");
-			setSelection(this.langs, this.namespace);
-		}
+		$(document).trigger(this.namespace, { langs: this.langs });
 	},
 	add: function(value) {
 		if($.inArray(value, this.langs) === -1) {
@@ -109,9 +84,7 @@ jQuery(document).ready(function($) {
 	};
 
 	$(document).bind("lang_selected", function(ev, data) {
-		if(data.langs.length) {
-			toggleSections(data.langs);
-		}
+		toggleSections(data.langs);
 	});
 	new IQVOC.LanguageSelector(container, "lang_selected");
 });
