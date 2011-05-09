b33c486880b76b40c8401c01c994e70adb76a9d5
Robert Glaser <mail@robert-glaser.de>
2011-05-09 16:07:12 +0200

    Removed manual setup of I18n fallbacks. It's already included in the Rails config.

--- config/initializers/iqvoc.rb
+++ config/initializers/iqvoc.rb
@@ -21,10 +21,3 @@ require 'iqvoc/deep_cloning'
 require 'iqvoc/rdf_helper'
 
 ActiveRecord::Base.send :include, Iqvoc::DeepCloning
-
-# Set the default locale to the first configured PrefLabeling language
-Rails.application.config.i18n.default_locale = Iqvoc::Concept.pref_labeling_languages.first
-
-# Turn on i18n fallback feature
-require "i18n/backend/fallbacks" 
-I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
---
+++ config/initializers/language.rb
@@ -0,0 +1,6 @@
+# Set the default locale to the first configured PrefLabeling language
+Rails.application.config.i18n.default_locale = Iqvoc::Concept.pref_labeling_languages.first
+
+# # Turn on i18n fallback feature
+# require "i18n/backend/fallbacks" 
+# I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
