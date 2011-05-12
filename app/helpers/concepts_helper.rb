6d12e1f1b0a9860b89ea0ada6adcb1ee885d98a6
Robert Glaser <mail@robert-glaser.de>
2011-05-12 16:15:51 +0200

    Abstracted the letter selector for DRYness. The route to use for a letter link can be passed via a block.

--- app/helpers/concepts_helper.rb
+++ app/helpers/concepts_helper.rb
@@ -65,23 +65,17 @@ module ConceptsHelper
   end
   
   def letter_selector(&block)
-    @alphas =
+    letters =
       ('A'..'Z').to_a +
       (0..9).to_a +
       ['[']
-    
-  # <ul class="letter_selector">
-  #   <% for alpha in @alphas %>
-  #   <%= tag("li", { :class => ((params[:letter] == alpha.to_s.downcase) ? "current" : nil) }, true) %>
-  #     <%= link_to alpha, untranslated_concepts_path(:letter => alpha.to_s.downcase) %>
-  #   </li>
-  #   <% end %>
-  # </ul>
   
     content_tag :ul, :class => 'letter_selector' do
-      @alphas.map do |alpha|
-        content_tag :li, :class => ((params[:letter] == alpha.to_s.downcase) ? "current" : nil) 
+      html = ""
+      letters.each do |letter|
+        html += content_tag(:li, link_to(letter, yield(letter)), :class => ((params[:letter] == letter.to_s.downcase) ? "current" : nil))
       end
+      html.html_safe
     end
   end
 
