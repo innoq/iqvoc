6c691ccbec909b6d930be70c9b96e7b34889ba99
Robert Glaser <mail@robert-glaser.de>
2011-05-25 10:22:36 +0200

    Removed setting of I18n.default_locale.

--- config/initializers/language.rb
+++ config/initializers/language.rb
@@ -1,5 +1,5 @@
 # Set the default locale to the first configured PrefLabeling language
-Rails.application.config.i18n.default_locale = Iqvoc::Concept.pref_labeling_languages.first
+# Rails.application.config.i18n.default_locale = Iqvoc::Concept.pref_labeling_languages.first ? Iqvoc::Concept.pref_labeling_languages.first : ""
 
 # # Turn on i18n fallback feature
 # require "i18n/backend/fallbacks"
