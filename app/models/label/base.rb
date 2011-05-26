3c9f4736d9686df73246b7315b23ccd2a504c990
Robert Glaser <mail@robert-glaser.de>
2011-05-26 16:57:20 +0200

    Check language if it gets passed as an empty string. In that case, query for NULL. WARNING: This seems to make the query horribly slow, has to be tuned.

--- app/models/label/base.rb
+++ app/models/label/base.rb
@@ -37,6 +37,8 @@ class Label::Base < ActiveRecord::Base
   scope :by_language, lambda { |lang_code|
     if (lang_code.is_a?(Array) && lang_code.include?(nil))
       where(arel_table[:language].eq(nil).or(arel_table[:language].in(lang_code.compact)))
+    elsif lang_code.blank?
+      where(arel_table[:language].eq(nil))
     else
       where(:language => lang_code)
     end
