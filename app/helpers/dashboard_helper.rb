806c0dd27f22941db87c9351b327b0a512095cc3
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2011-06-14 17:04:36 +0200

    Added some funky code

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -18,22 +18,18 @@ module DashboardHelper
 
   def sorting_arrows_for(name)
     content_tag :div, :class => "sorting_arrows" do
-      (
-        link_to(image_tag("iqvoc/arrow_down.gif", :class => "arrow_down"),
-          dashboard_path(:order => "asc", :by => name.to_s)) +
+      link_to(image_tag("iqvoc/arrow_down.gif", :class => "arrow_down"),
+        dashboard_path(:order => "asc", :by => name.to_s)) +
         link_to(image_tag("iqvoc/arrow_up.gif", :class => "arrow_up"),
-          dashboard_path(:order => "desc", :by => name.to_s))
-      )
+        dashboard_path(:order => "desc", :by => name.to_s))
     end
   end
 
   def consistency_status(item)
-    if item.valid_with_full_validation?
-      css = "valid"
-      msg = "&#x2713;"
+    css, msg = if item.valid_with_full_validation?
+      ["valid", "&#x2713;"]
     else
-      css = "invalid"
-      msg = "&#x2717;"
+      ["invalid", "&#x2717;"]
     end
 
     content_tag :span, raw(msg), :class => css
