d297b070621fc628444354dfdd59628e563492d2
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2011-06-14 13:06:15 +0200

    only set back_to in user login redirects on authorization error and redirect after successful login to dashboard by default

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -15,25 +15,33 @@
 # limitations under the License.
 
 class UserSessionsController < ApplicationController
+  
   skip_before_filter :require_user, :only => [:new, :create]
 
   def new
+    # TODO Check abilitys
+
     @user_session = UserSession.new
   end
 
   def create
+    # TODO Check abilitys
+
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
       flash[:notice] = I18n.t("txt.controllers.user_sessions.login_success")
-      redirect_to params[:back_to] || root_path
+      redirect_to params[:back_to] || (can?(:use, :dashboard) ? dashboard_url : root_path)
     else
       render :action => :new
     end
   end
 
   def destroy
+    # TODO Check abilitys
+
     current_user_session.destroy
     flash[:notice] = I18n.t("txt.controllers.user_sessions.logout_success")
     redirect_to root_path
   end
+
 end
