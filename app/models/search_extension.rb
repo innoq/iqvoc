7eaaf24e90ea1f0046ab7905c5dda899fe63a17a
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2011-06-15 17:34:28 +0200

    Fixed an abuse of ActiveSupport::Concern and added some dryness

--- app/models/search_extension.rb
+++ app/models/search_extension.rb
@@ -17,8 +17,13 @@
 module SearchExtension
   extend ActiveSupport::Concern
 
-  included do
-    def self.multi_query(params = {})
+  def build_search_result_rdf(document, result)
+    raise NotImplementedError.new("Implement build_search_result_rdf in your specific class (#{self.class.name}) that should be searchable!")
+  end
+
+  module ClassMethods
+    
+    def multi_query(params = {})
       query_terms = params[:query].split(/\r\n/)
       results     = []
       query_terms.each do |term|
@@ -27,43 +32,27 @@ module SearchExtension
       results
     end
 
-    def self.single_query(params = {})
+    def single_query(params = {})
       raise NotImplementedError.new("Implement self.single_query in your specific class (#{self.name}) that should be searchable!")
     end
 
-    def self.supports_multi_query?
-      false # FIXME Multipquerys don't work with will_paginate! Perhaps we schould remove them completely?
-    end
-
-    def self.forces_multi_query?
+    def supports_multi_query?
       false
     end
 
-    def self.build_query_string(params = {})
-      query_type = params[:query_type] || 'contains'
+    def forces_multi_query?
+      false
+    end
 
-      query_str = case query_type
-      when 'contains'
-        "%#{params[:query]}%"
-      when 'begins_with'
-        "#{params[:query]}%"
-      when 'ends_with'
-        "%#{params[:query]}"
-        # when 'regexp'
-        #   params[:query]
-      when 'exact'
-        params[:query]
-      else
-        params[:query]
-      end
+    def build_query_string(params = {})
+      query_str = params[:query]
+      query_str = "%#{query_str}" if ["contains", "ends_with"].include?(params[:query_type].to_s)
+      query_str = "#{query_str}%" if ["contains", "begins_with"].include?(params[:query_type].to_s)
+      # Note that 'contains' will add an '%' to the beginning AND to the end
 
       query_str
     end
 
-  end
-
-  def build_search_result_rdf(document, result)
-    raise NotImplementedError.new("Implement build_search_result_rdf in your specific class (#{self.class.name}) that should be searchable!")
   end
 
 end
