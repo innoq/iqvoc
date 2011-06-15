5431b3a44c2bbcf48bb0a9062d2cdc6daa63217a
FND <fnd@fnd-oda.(none)>
2011-06-15 13:40:03 +0200

    moved instance settings into existing initializer
    since we're not trying to be idiot-proof anymore, there's no need for a
    separate file

--- config/initializers/iqvoc.rb
+++ config/initializers/iqvoc.rb
@@ -23,3 +23,25 @@ require 'iqvoc/rdf_helper'
 require 'iqvoc/ability'
 
 ActiveRecord::Base.send :include, Iqvoc::DeepCloning
+
+
+##### INSTANCE SETTINGS #####
+
+# uncomment the settings below and adjust as desired
+# see lib/iqvoc.rb for the full list of available setting
+
+if Rails.env != "test"
+
+  #Iqvoc.title = "My Thesaurus"
+
+  # custom assets
+  #Iqvoc.additional_js_files  += ["vendor/myScripts.js"]
+  #Iqvoc.additional_css_files += ["vendor/myStyles.css"]
+
+  # label languages (and classes)
+  #Iqvoc::Concept.pref_labeling_languages      = [ :de, :en ]
+  #Iqvoc::Concept.further_labeling_class_names = {
+  #  "Labeling::SKOS::AltLabel" => [ :de, :en ]
+  #}
+
+end
