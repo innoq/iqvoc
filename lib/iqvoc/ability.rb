af43ecb35339cd0a72c49b906eb7ff7ca965a98d
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2011-06-16 10:59:13 +0200

    DRYed out the abilities

--- lib/iqvoc/ability.rb
+++ lib/iqvoc/ability.rb
@@ -8,43 +8,34 @@ module Iqvoc
 
       can :read, ::Collection::Base
 
-      if user.nil?
-        can :read, [::Concept::Base, ::Label::Base], &@@if_published
-      end
+      can :read, [::Concept::Base, ::Label::Base], &@@if_published
 
-      if user && user.owns_role?(:reader)
-        can :read, [::Concept::Base, ::Label::Base], &@@if_published
-
-        can :use, :dashboard
-      end
-
-      if user && (user.owns_role?(:editor) || user.owns_role?(:publisher) || user.owns_role?(:administrator))
-        can :read, [::Concept::Base, ::Label::Base]
-
+      if user # Every logged in user ...
         can :use, :dashboard
 
-        can :manage, ::Collection::Base
+        if user.owns_role?(:editor) || user.owns_role?(:publisher) || user.owns_role?(:administrator) # Editors and above ...
+          can :manage, ::Collection::Base
 
-        can :create, [::Concept::Base, ::Label::Base]
-        can [:update, :destroy, :unlock], [::Concept::Base, ::Label::Base], :locked_by => user.id, :published_at => nil
-        can :lock, [::Concept::Base, ::Label::Base], :locked_by => nil, :published_at => nil
+          can :read, [::Concept::Base, ::Label::Base]
+          can :create, [::Concept::Base, ::Label::Base]
+          can [:update, :destroy, :unlock], [::Concept::Base, ::Label::Base], :locked_by => user.id, :published_at => nil
+          can :lock, [::Concept::Base, ::Label::Base], :locked_by => nil, :published_at => nil
+          can [:check_consistency, :send_to_review], [::Concept::Base, ::Label::Base], :published_at => nil
+          can :branch, [::Concept::Base, ::Label::Base], &@@if_published
+        end
 
-        can :check_consistency, [::Concept::Base, ::Label::Base], :published_at => nil
-        can :send_to_review, [::Concept::Base, ::Label::Base], :published_at => nil
+        if user.owns_role?(:publisher) || user.owns_role?(:administrator) # Publishers and above ...
+          can :merge, [::Concept::Base, ::Label::Base], :published_at => nil
+        end
 
-        can :branch, [::Concept::Base, ::Label::Base], &@@if_published
-      end
+        if user.owns_role?(:administrator) # Admins ...
+          can [:update, :destroy, :unlock], [::Concept::Base, ::Label::Base], :published_at => nil # Mustn't be locked by myself
 
-      if user && (user.owns_role?(:publisher) || user.owns_role?(:administrator))
-        can :merge, [::Concept::Base, ::Label::Base], :published_at => nil
-      end
+          can :manage, User
 
-      if user && user.owns_role?(:administrator)
-        can [:update, :destroy, :unlock], [::Concept::Base, ::Label::Base], :published_at => nil # Mustn't be locked by myself
-
-        can :manage, User
-
-        can :full_export, ::Concept::Base
+          can :full_export, ::Concept::Base
+        end
+      
       end
 
     end
