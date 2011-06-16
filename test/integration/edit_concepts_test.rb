fd07e82e0f8b2f8ded17cf238a6b962922b5a8dc
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2011-06-16 15:56:40 +0200

    Fixed a bug in the tests. current_path shouldn't be used to be compared with a value from a *_path helper because they are working different: Capibara returns the path segment without parameters (it cuts off everything behind the "?").

--- test/integration/edit_concepts_test.rb
+++ test/integration/edit_concepts_test.rb
@@ -28,13 +28,13 @@ class EditConceptsTest < ActionDispatch::IntegrationTest
     visit concept_path(@concept, :lang => 'de', :format => 'html')
     assert page.has_button?("Neue Version erstellen"), "Button 'Neue Version erstellen' is missing on concepts#show"
     click_link_or_button("Neue Version erstellen")
-    assert_equal edit_concept_path(@concept, :lang => 'de', :format => 'html'), current_path
+    assert_equal edit_concept_url(@concept, :published => 0, :lang => 'de', :format => 'html'), current_url
 
     visit concept_path(@concept, :lang => 'de', :format => 'html')
     assert !page.has_button?("Neue Version erstellen"), "Button 'Neue Version erstellen' although there already is a new version"
     assert page.has_link?("Vorschau der Version in Bearbeitung"), "Link 'Vorschau der Version in Bearbeitung' is missing"
-    click_link_or_button("Vorschau der Version in Bearbeitung")
-    # Doesn't work... Capibara bug???  assert_equal concept_path(@concept, :published => 0, :lang => 'de', :format => 'html'), current_path
+    click_link("Vorschau der Version in Bearbeitung")
+    assert_equal concept_url(@concept, :published => 0, :lang => 'de', :format => 'html'), current_url
   end
 
 end
