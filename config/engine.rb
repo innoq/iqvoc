7c1a73485dd2518ab05fabf86f1360e3d10334ec
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2011-06-16 09:48:12 +0200

    Fixed a comment which wasn't a real TODO

--- config/engine.rb
+++ config/engine.rb
@@ -14,7 +14,7 @@ module Iqvoc
 
     paths.lib.tasks  << "lib/engine_tasks"
 
-    # TODO Will be defined in Rails 3.1 (as well as the tasks in lib/engine_tasks)
+    # This will be defined in Rails 3.1 (as well as all the tasks in lib/engine_tasks)
     def self.load_seed
       seed_file = Iqvoc::Engine.find_root_with_flag("db").join('db/seeds.rb')
       load(seed_file) if File.exist?(seed_file)
