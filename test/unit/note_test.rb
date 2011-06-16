9581d1a3085126b1e7e5e491a70e7b36ea1e37e2
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2011-06-16 12:12:54 +0200

    Made the unit tests directly runnable

--- test/unit/note_test.rb
+++ test/unit/note_test.rb
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require 'test_helper'
+require File.join(File.expand_path(File.dirname(__FILE__)), '../test_helper')
 
 class NoteTest < ActiveSupport::TestCase
 
