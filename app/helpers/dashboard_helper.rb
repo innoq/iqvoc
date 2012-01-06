e86b8eadc0d0614db99b32fd58fa937bd5fd1dfc
Robert Glaser <mail@robert-glaser.de>
2012-01-06 13:12:43 +0100

    Fixed arrow link image asset paths.

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -18,9 +18,9 @@ module DashboardHelper
 
   def sorting_arrows_for(name)
     content_tag :div, :class => "sorting_arrows" do
-      link_to(image_tag("iqvoc/arrow_down.gif", :class => "arrow_down"),
+      link_to(image_tag("arrow_down.gif", :class => "arrow_down"),
         dashboard_path(:order => "asc", :by => name.to_s)) +
-        link_to(image_tag("iqvoc/arrow_up.gif", :class => "arrow_up"),
+        link_to(image_tag("arrow_up.gif", :class => "arrow_up"),
         dashboard_path(:order => "desc", :by => name.to_s))
     end
   end
