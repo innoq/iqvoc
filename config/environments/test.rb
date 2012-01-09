6d597d16566e91d8fcad295e60da064b5ad214d9
Robert Glaser <mail@robert-glaser.de>
2012-01-09 09:07:55 +0100

    Whitespace.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -35,7 +35,7 @@ if Iqvoc.const_defined?(:Application)
     config.action_dispatch.show_exceptions = false
 
     # Disable request forgery protection in test environment
-    config.action_controller.allow_forgery_protection    = false
+    config.action_controller.allow_forgery_protection = false
 
     # Tell Action Mailer not to deliver emails to the real world.
     # The :test delivery method accumulates sent emails in the
