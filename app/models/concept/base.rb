b1d7d2903e50171c535a1b011717c43029189cc0
FND <fnd@fnd-oda.(none)>
2012-01-31 13:30:31 +0100

    added explicit dependency handling to work around LoadError exceptions
    without this, the following exceptions are thrown arbitrarily:
        Expected app/models/concept/skos/base.rb
            to define Concept::SKOS::Base
        Expected app/models/concept/relation/reverse_relation_extension.rb
            to define Concept::Relation::ReverseRelationExtension
    
    tests still complain though; TBD

--- app/models/concept/base.rb
+++ app/models/concept/base.rb
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+require 'concept/relation/reverse_relation_extension'
+
 class Concept::Base < ActiveRecord::Base
 
   self.table_name = 'concepts'
