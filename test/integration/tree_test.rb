c09b3c1e13b2bb44f967c43de49d8708d6465eb4
Robert Glaser <mail@robert-glaser.de>
2012-02-07 13:43:42 +0100

    reformatting

--- test/integration/tree_test.rb
+++ test/integration/tree_test.rb
@@ -23,8 +23,10 @@ class TreeTest < ActionDispatch::IntegrationTest
     narrower_concept = concept.narrower_relations.first.target
 
     visit hierarchical_concepts_path(:lang => :de, :format => :html)
-    assert page.has_link?(concept.pref_label.to_s), "Concept #{concept.pref_label} isn't visible in the hierarchical concepts list"
-    assert !page.has_content?(narrower_concept.pref_label.to_s), "Narrower relation (#{narrower_concept.pref_label}) shouldn't be visible in the hierarchical concepts list"
+    assert page.has_link?(concept.pref_label.to_s), 
+      "Concept #{concept.pref_label} isn't visible in the hierarchical concepts list"
+    assert !page.has_content?(narrower_concept.pref_label.to_s), 
+      "Narrower relation (#{narrower_concept.pref_label}) shouldn't be visible in the hierarchical concepts list"
   end
 
 end
