7625e4dc0b718f8c82cf7f2417f31a63741693c0
FND <fnd@fnd-oda.(none)>
2012-02-09 11:49:34 +0100

    added basic tests for code hygiene (whitespace)
    I expect many complaints from fellow contributors
    
    NB: intentionally letting these tests fail for now to see how Travis CI
    handles it

---
+++ test/unit/hygiene_test.rb
@@ -0,0 +1,21 @@
+# encoding: UTF-8
+
+class PostalCodeTest < ActiveSupport::TestCase
+
+  test "trailing whitespace" do
+    assert_no_occurrence '[[:blank:]]$', "trailing whitespace"
+  end
+
+  test "mixed whitespace" do
+    tab = "	"
+    space = " "
+    assert_no_occurrence "#{space}#{tab}\|#{tab}#{space}", "mixed whitespace", true
+  end
+
+  def assert_no_occurrence(pattern, error_message, extended=false)
+    options = extended ? "-IE" : "-I"
+    lines = `git grep #{options} '#{pattern}' | grep -v '^vendor/'`
+    assert_not_equal 0, $?.to_i, "#{error_message}:\n#{lines}"
+  end
+
+end
