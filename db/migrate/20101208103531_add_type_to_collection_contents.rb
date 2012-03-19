c2130c07ff469620a9ed43687953cf4ffb1509d0
Robert Glaser <mail@robert-glaser.de>
2012-03-19 12:58:22 +0100

    trying to fix ddl transaction bugs happening with pg

--- db/migrate/20101208103531_add_type_to_collection_contents.rb
+++ db/migrate/20101208103531_add_type_to_collection_contents.rb
@@ -18,6 +18,10 @@ class AddTypeToCollectionContents < ActiveRecord::Migration
   def self.up
     rename_column :collection_members, :concept_id, :target_id
     add_column :collection_members, :type, :string
+    
+    Collection::Member::Base.reset_column_informtion
+    Collection::Base.reset_column_informtion
+    
     Collection::Member::Base.update_all(:type => 'Collection::Member::Concept')
     Collection::Base.update_all(:type => 'Collection::Base')
   end
