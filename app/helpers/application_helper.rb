ce8eed82b138bd0fe3fa2226814016b219becb04
Robert Glaser <mail@robert-glaser.de>
2012-03-26 15:17:24 +0200

    whitespace

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -37,10 +37,10 @@ module ApplicationHelper
 
   def iqvoc_default_rdf_namespaces
     Iqvoc.rdf_namespaces.merge({
-        :default => root_url(:format => nil, :lang => nil, :trailing_slash => true).gsub(/\/\/$/, "/"), # gsub because of a Rails bug :-(
-        :coll => collections_url(:trailing_slash => true, :lang => nil, :format => nil),
-        :schema => controller.schema_url(:format => nil, :anchor => "", :lang => nil)
-      })
+      :default => root_url(:format => nil, :lang => nil, :trailing_slash => true).gsub(/\/\/$/, "/"), # gsub because of a Rails bug :-(
+      :coll => collections_url(:trailing_slash => true, :lang => nil, :format => nil),
+      :schema => controller.schema_url(:format => nil, :anchor => "", :lang => nil)
+    })
   end
 
   def options_for_language_select(selected = nil)
