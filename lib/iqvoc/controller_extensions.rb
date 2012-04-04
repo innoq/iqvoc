5c531d861eea8fbebc02f461fa423da6bf47bc78
Robert Glaser <mail@robert-glaser.de>
2012-04-04 12:59:06 +0200

    improve caching syntax for current user stuff

--- lib/iqvoc/controller_extensions.rb
+++ lib/iqvoc/controller_extensions.rb
@@ -89,13 +89,11 @@ module Iqvoc
     end
 
     def current_user_session
-      return @current_user_session if defined?(@current_user_session)
-      @current_user_session = UserSession.find
+      @current_user_session ||= UserSession.find
     end
 
     def current_user
-      return @current_user if defined?(@current_user)
-      @current_user = current_user_session && current_user_session.user
+      @current_user ||= current_user_session && current_user_session.user
     end
 
   end
