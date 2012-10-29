ea78cb19b08f679b69c75d3f150fe1fdfaa13d0e
FND <fnd@localhost.localdomain>
2012-10-29 08:56:05 +0100

    fixed quotation mark consistency

--- config/initializers/language.rb
+++ config/initializers/language.rb
@@ -2,5 +2,5 @@
 # Rails.application.config.i18n.default_locale = Iqvoc::Concept.pref_labeling_languages.first ? Iqvoc::Concept.pref_labeling_languages.first : ""
 
 # # Turn on i18n fallback feature
-# require "i18n/backend/fallbacks"
+# require 'i18n/backend/fallbacks'
 # I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
