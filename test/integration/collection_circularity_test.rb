a8792144d058866d8ba36f733be3e0912ebe26b1
Willem van Kerkhof <wvk@consolving.de>
2013-02-27 23:19:22 +0100

    Get rid of FactoryGirl in a first integration test.

--- test/integration/collection_circularity_test.rb
+++ test/integration/collection_circularity_test.rb
@@ -16,95 +16,101 @@
 
 require File.join(File.expand_path(File.dirname(__FILE__)), '../integration_test_helper')
 
+API = Iqvoc::RDFAPI
+
 class CollectionCircularityTest < ActionDispatch::IntegrationTest
-
   setup do
-    @coll1 = FactoryGirl.create(:collection)
-    @coll2 = FactoryGirl.create(:collection)
-    @coll3 = FactoryGirl.create(:collection)
-    @concept1 = FactoryGirl.create(:concept)
-    @concept2 = FactoryGirl.create(:concept)
-    @concept3 = FactoryGirl.create(:concept)
+    API.parse_triples <<-EOT
+      :coll1 rdf:type skos:Collection
+      :coll1 skos:prefLabel "Collection 1"@en
+      :coll2 rdf:type skos:Collection
+      :coll2 skos:prefLabel "Collection 2"@en
+      :coll3 rdf:type skos:Collection
+      :coll3 skos:prefLabel "Collection 3"@en
+
+      :concept1 rdf:type skos:Concept
+      :concept1 skos:prefLabel "Concept 1"@en
+      :concept2 rdf:type skos:Concept
+      :concept2 skos:prefLabel "Concept 3"@en
+      :concept3 rdf:type skos:Concept
+      :concept3 skos:prefLabel "Concept 3"@en
+    EOT
   end
 
-  test "inline assignments are persisted" do
-    login("administrator")
+  test 'inline assignments are persisted' do
+    login('administrator')
 
-    delimiter = "," # without space
+    visit edit_collection_path(API.cached('coll1'), :lang => 'en', :format => 'html')
+    fill_in 'concept_inline_member_collection_origins',
+        :with => 'coll2,coll3' # without space
+    fill_in 'concept_inline_member_concept_origins',
+        :with => 'concept1,concept2,concept3' # without space
+    click_button 'Save'
 
-    visit edit_collection_path(@coll1, :lang => "en", :format => "html")
-    fill_in "concept_inline_member_collection_origins",
-        :with => [@coll2.origin, @coll3.origin].join(delimiter)
-    fill_in "concept_inline_member_concept_origins",
-        :with => [@concept1.origin, @concept2.origin, @concept3.origin].join(delimiter)
-    click_button "Save"
+    assert page.has_no_css?('.flash_error')
+    assert page.has_content?(I18n.t('txt.controllers.collections.save.success'))
+    assert page.has_link?(API.cached('coll2').pref_label.to_s)
+    assert page.has_link?(API.cached('coll3').pref_label.to_s)
+    assert page.has_link?(API.cached('concept1').pref_label.to_s)
+    assert page.has_link?(API.cached('concept2').pref_label.to_s)
+    assert page.has_link?(API.cached('concept3').pref_label.to_s)
 
-    assert page.has_no_css?(".flash_error")
-    assert page.has_content?(I18n.t("txt.controllers.collections.save.success"))
-    assert page.has_link?(@coll2.pref_label.to_s)
-    assert page.has_link?(@coll3.pref_label.to_s)
-    assert page.has_link?(@concept1.pref_label.to_s)
-    assert page.has_link?(@concept2.pref_label.to_s)
-    assert page.has_link?(@concept3.pref_label.to_s)
+    click_link_or_button 'Edit'
+    fill_in 'concept_inline_member_collection_origins', :with => ''
+    fill_in 'concept_inline_member_concept_origins', :with => ''
+    click_button 'Save'
 
-    click_link_or_button "Edit"
-    fill_in "concept_inline_member_collection_origins", :with => ""
-    fill_in "concept_inline_member_concept_origins", :with => ""
-    click_button "Save"
+    assert page.has_no_css?('.flash_error')
+    assert page.has_content?(I18n.t('txt.controllers.collections.save.success'))
+    assert page.has_no_link?(API.cached('coll2').pref_label.to_s)
+    assert page.has_no_link?(API.cached('coll3').pref_label.to_s)
+    assert page.has_no_link?(API.cached('concept1').pref_label.to_s)
+    assert page.has_no_link?(API.cached('concept2').pref_label.to_s)
+    assert page.has_no_link?(API.cached('concept3').pref_label.to_s)
 
-    assert page.has_no_css?(".flash_error")
-    assert page.has_content?(I18n.t("txt.controllers.collections.save.success"))
-    assert page.has_no_link?(@coll2.pref_label.to_s)
-    assert page.has_no_link?(@coll3.pref_label.to_s)
-    assert page.has_no_link?(@concept1.pref_label.to_s)
-    assert page.has_no_link?(@concept2.pref_label.to_s)
-    assert page.has_no_link?(@concept3.pref_label.to_s)
+    click_link_or_button 'Edit'
+    fill_in 'concept_inline_member_collection_origins',
+        :with => 'coll2, coll3' # with space
+    fill_in 'concept_inline_member_concept_origins',
+        :with => 'concept1, concept2, concept3' # with space
+    click_button 'Save'
 
-    delimiter = ", " # with space
-
-    click_link_or_button "Edit"
-    fill_in "concept_inline_member_collection_origins",
-        :with => [@coll2.origin, @coll3.origin].join(delimiter)
-    fill_in "concept_inline_member_concept_origins",
-        :with => [@concept1.origin, @concept2.origin, @concept3.origin].join(delimiter)
-    click_button "Save"
-
-    assert page.has_no_css?(".flash_error")
-    assert page.has_content?(I18n.t("txt.controllers.collections.save.success"))
-    assert page.has_link?(@coll2.pref_label.to_s)
-    assert page.has_link?(@coll3.pref_label.to_s)
-    assert page.has_link?(@concept1.pref_label.to_s)
-    assert page.has_link?(@concept2.pref_label.to_s)
-    assert page.has_link?(@concept3.pref_label.to_s)
+    assert page.has_no_css?('.flash_error')
+    assert page.has_content?(I18n.t('txt.controllers.collections.save.success'))
+    assert page.has_link?(API.cached('coll2').pref_label.to_s)
+    assert page.has_link?(API.cached('coll3').pref_label.to_s)
+    assert page.has_link?(API.cached('concept1').pref_label.to_s)
+    assert page.has_link?(API.cached('concept2').pref_label.to_s)
+    assert page.has_link?(API.cached('concept3').pref_label.to_s)
   end
 
-  test "circular sub-collection references are rejected during update" do
-    login("administrator")
+  test 'circular sub-collection references are rejected during update' do
+    login('administrator')
 
     # add coll2 as subcollection of coll1
-    visit edit_collection_path(@coll1, :lang => "en", :format => "html")
-    fill_in "concept_inline_member_collection_origins",
-        :with => "%s," % @coll2.origin
-    click_button "Save"
+    visit edit_collection_path(API.cached('coll1'), :lang => 'en', :format => 'html')
+    fill_in 'concept_inline_member_collection_origins',
+        :with => 'coll2,'
+    click_button 'Save'
 
-    assert page.has_no_css?(".flash_error")
-    assert page.has_content?(I18n.t("txt.controllers.collections.save.success"))
-    assert page.has_link?(@coll2.pref_label.to_s,
-        :href => collection_path(@coll2, :lang => "en", :format => "html"))
+    assert page.has_no_css?('.flash_error')
+    assert page.has_content?(I18n.t('txt.controllers.collections.save.success'))
+    assert page.has_link?(API.cached('coll2').pref_label.to_s,
+        :href => collection_path(API.cached('coll2'), :lang => 'en', :format => 'html'))
 
     # add coll1 as subcollection of coll2
-    visit edit_collection_path(@coll2, :lang => "en", :format => "html")
-    fill_in "concept_inline_member_collection_origins",
-        :with => "%s," % @coll1.origin
-    click_button "Save"
+    visit edit_collection_path(API.cached('coll2'), :lang => 'en', :format => 'html')
+    fill_in 'concept_inline_member_collection_origins',
+        :with => 'coll1,'
+    click_button 'Save'
 
-    assert page.has_css?(".alert-error")
-    assert page.has_css?("#edit_concept")
+    assert page.has_css?('.alert-error')
+    assert page.has_css?('#edit_concept')
     assert page.has_content?( # XXX: page.has_content? didn't work
-        I18n.t("txt.controllers.collections.circular_error", :label => @coll1.pref_label))
+        I18n.t('txt.controllers.collections.circular_error', :label => API.cached('coll1').pref_label))
 
     # ensure coll1 is not a subcollection of coll2
-    visit collection_path(@coll2, :lang => "en", :format => "html")
-    assert page.has_no_css?(".relation ul.treeview li") # XXX: too unspecific
+    visit collection_path(API.cached('coll2'), :lang => 'en', :format => 'html')
+    assert page.has_no_css?('.relation ul.treeview li') # XXX: too unspecific
   end
 end
