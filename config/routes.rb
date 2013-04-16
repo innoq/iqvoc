87811ea597acb463d24ea4fde0f404b81aaee22c
Robert Glaser <mail@robert-glaser.de>
2013-04-16 17:18:48 +0200

    concise use of quotation marks

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,21 @@
 
 # Copyright 2011 innoQ Deutschland GmbH
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# distributed under the License is distributed on an 'AS IS' BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
 Rails.application.routes.draw do
   scope ':lang', :constraints => lambda { |params, req|
-    langs = Iqvoc::Concept.pref_labeling_languages.join("|").presence || "en"
+    langs = Iqvoc::Concept.pref_labeling_languages.join('|').presence || 'en'
     return params[:lang].to_s =~ /^#{langs}$/
   } do
 
@@ -27,23 +27,23 @@ Rails.application.routes.draw do
     resources :concepts
     resources :collections
 
-    get "scheme" => "concepts/scheme#show", :as => "scheme"
-    get "scheme/edit" => "concepts/scheme#edit", :as => "edit_scheme"
-    put "scheme" => "concepts/scheme#update"
+    get 'scheme' => 'concepts/scheme#show', :as => 'scheme'
+    get 'scheme/edit' => 'concepts/scheme#edit', :as => 'edit_scheme'
+    put 'scheme' => 'concepts/scheme#update'
 
-    get "hierarchy/:root" => "hierarchy#show"
+    get 'hierarchy/:root' => 'hierarchy#show'
 
-    get "triplestore_sync" => "triplestore_sync#index"
-    post "triplestore_sync" => "triplestore_sync#sync"
+    get 'triplestore_sync' => 'triplestore_sync#index'
+    post 'triplestore_sync' => 'triplestore_sync#sync'
 
-    match "/" => "frontpage#index"
+    match '/' => 'frontpage#index'
 
-    match "concepts/:origin/branch(.:format)"      => "concepts/versions#branch",    :as => "concept_versions_branch"
-    match "concepts/:origin/merge(.:format)"       => "concepts/versions#merge",     :as => "concept_versions_merge"
-    match "concepts/:origin/lock(.:format)"        => "concepts/versions#lock",      :as => "concept_versions_lock"
-    match "concepts/:origin/unlock(.:format)"      => "concepts/versions#unlock",    :as => "concept_versions_unlock"
-    match "concepts/:origin/to_review(.:format)"   => "concepts/versions#to_review", :as => "concept_versions_to_review"
-    match "concepts/:origin/consistency_check(.:format)" => "concepts/versions#consistency_check", :as => "concept_versions_consistency_check"
+    match 'concepts/:origin/branch(.:format)'      => 'concepts/versions#branch',    :as => 'concept_versions_branch'
+    match 'concepts/:origin/merge(.:format)'       => 'concepts/versions#merge',     :as => 'concept_versions_merge'
+    match 'concepts/:origin/lock(.:format)'        => 'concepts/versions#lock',      :as => 'concept_versions_lock'
+    match 'concepts/:origin/unlock(.:format)'      => 'concepts/versions#unlock',    :as => 'concept_versions_unlock'
+    match 'concepts/:origin/to_review(.:format)'   => 'concepts/versions#to_review', :as => 'concept_versions_to_review'
+    match 'concepts/:origin/consistency_check(.:format)' => 'concepts/versions#consistency_check', :as => 'concept_versions_consistency_check'
 
     match 'alphabetical_concepts(/:prefix)(.:format)' => 'concepts/alphabetical#index', :as => 'alphabetical_concepts'
     match 'untranslated_concepts/:prefix(.:format)'   => 'concepts/untranslated#index', :as => 'untranslated_concepts'
@@ -55,12 +55,12 @@ Rails.application.routes.draw do
     get 'config(.:format)' => 'instance_configuration#index', :as => 'instance_configuration'
     put 'config(.:format)' => 'instance_configuration#update'
 
-    get "import" => "import#index", :as => 'import'
-    post "import" => "import#import"
+    get 'import' => 'import#index', :as => 'import'
+    post 'import' => 'import#import'
 
     match 'search(.:format)' => 'search_results#index', :as => 'search'
 
-    get "help" => "pages#help", :as => "help"
+    get 'help' => 'pages#help', :as => 'help'
 
     root :to => 'frontpage#index', :format => nil
   end
@@ -72,8 +72,8 @@ Rails.application.routes.draw do
 
   get ':id(.:format)' => 'rdf#show', :as => 'rdf'
 
-  get 'collections/:id(.:format)', :as => "rdf_collection", :to => "collections#show"
-  get 'collections', :as => "rdf_collections", :to => "collections#index"
+  get 'collections/:id(.:format)', :as => 'rdf_collection', :to => 'collections#show'
+  get 'collections', :as => 'rdf_collections', :to => 'collections#index'
 
   root :to => 'frontpage#index', :format => nil
 end
