6373fd9a27af81e8829d75b7668f6a41243b3486
Robert Glaser <mail@robert-glaser.de>
2013-04-16 16:58:32 +0200

    global config: don't initialize env on precompiling assets

--- config/application.rb
+++ config/application.rb
@@ -68,5 +68,7 @@ module Iqvoc
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.initialize_on_precompile = false
   end
 end
