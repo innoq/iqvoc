6718e18053fd59685f088f942f14185fc5e8a780
Robert Glaser <mail@robert-glaser.de>
2013-05-17 14:35:56 +0200

    extract capybara helper methods to separate file
    makes it easy for apps embedding iqvoc as an engine to require those

---
+++ test/capybara_helper.rb
@@ -0,0 +1,28 @@
+module ActionDispatch
+  class IntegrationTest
+    include Capybara::DSL
+
+    Capybara.javascript_driver = :webkit
+
+    def login(role = nil)
+      logout
+      user(role)
+      visit new_user_session_path(:lang => :de)
+      fill_in "E-Mail", :with => user.email
+      fill_in "Passwort", :with => user.password
+      click_button "Anmelden"
+    end
+
+    def logout
+      visit dashboard_path(:lang => :de)
+      click_link_or_button "Abmelden" if page.has_link?("Abmelden")
+      @user.try(:destroy)
+      @user = nil
+    end
+
+    def user(role = nil)
+      @user ||= FactoryGirl.create(:user, :role => (role || User.default_role))
+    end
+
+  end
+end
--- test/integration_test_helper.rb
+++ test/integration_test_helper.rb
@@ -16,32 +16,4 @@
 
 require File.join(File.expand_path(File.dirname(__FILE__)), 'test_helper')
 require 'capybara/rails'
-
-module ActionDispatch
-  class IntegrationTest
-    include Capybara::DSL
-
-    Capybara.javascript_driver = :webkit
-
-    def login(role = nil)
-      logout
-      user(role)
-      visit new_user_session_path(:lang => :de)
-      fill_in "E-Mail", :with => user.email
-      fill_in "Passwort", :with => user.password
-      click_button "Anmelden"
-    end
-
-    def logout
-      visit dashboard_path(:lang => :de)
-      click_link_or_button "Abmelden" if page.has_link?("Abmelden")
-      @user.try(:destroy)
-      @user = nil
-    end
-
-    def user(role = nil)
-      @user ||= FactoryGirl.create(:user, :role => (role || User.default_role))
-    end
-
-  end
-end
+require File.join(File.expand_path(File.dirname(__FILE__)), 'capybara_helper')
