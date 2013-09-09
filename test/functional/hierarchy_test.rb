bcd233b35bd87353707595576a0a38c04e3c295e
Robert Glaser <mail@robert-glaser.de>
2013-09-09 16:09:53 +0200

    fix order sensitive assertions for rdf responses

--- test/functional/hierarchy_test.rb
+++ test/functional/hierarchy_test.rb
@@ -160,33 +160,29 @@ boot:
     get :show, :lang => "en", :format => "ttl", :root => "root"
     assert_response 200
     assert_equal @response.content_type, "text/turtle"
-    assert @response.body.include?(<<-EOS)
-:root a skos:Concept;
-      skos:topConceptOf :scheme;
-      skos:prefLabel "Root"@en;
-      skos:narrower :foo;
-      skos:narrower :bar.
-    EOS
+    assert @response.body =~ /:root [^.]* skos:topConceptOf :scheme/m
+    assert @response.body =~ /:root [^.]* skos:prefLabel "Root"@en/m
+    assert @response.body =~ /:root [^.]* skos:narrower :bar/m
+    assert @response.body =~ /:root [^.]* skos:narrower :foo/m
+
     assert @response.body.include?(<<-EOS)
 :foo a skos:Concept;
      skos:prefLabel "Foo"@en.
     EOS
-    assert @response.body.include?(<<-EOS)
-:bar a skos:Concept;
-     skos:prefLabel "Bar"@en;
-     skos:narrower :alpha;
-     skos:narrower :bravo.
-    EOS
+
+    assert @response.body =~ /:bar [^.]* skos:prefLabel "Bar"@en/m
+    assert @response.body =~ /:bar [^.]* skos:narrower :alpha/m
+    assert @response.body =~ /:bar [^.]* skos:narrower :bravo/m
+
     assert @response.body.include?(<<-EOS)
 :alpha a skos:Concept;
        skos:prefLabel "Alpha"@en.
     EOS
-    assert @response.body.include?(<<-EOS)
-:bravo a skos:Concept;
-       skos:prefLabel "Bravo"@en;
-       skos:narrower :uno;
-       skos:narrower :dos.
-    EOS
+
+    assert @response.body =~ /:bravo [^.]* skos:prefLabel "Bravo"@en/m
+    assert @response.body =~ /:bravo [^.]* skos:narrower :uno/m
+    assert @response.body =~ /:bravo [^.]* skos:narrower :dos/m
+
     assert @response.body.include?(<<-EOS)
 :uno a skos:Concept;
      skos:prefLabel "Uno"@en.
