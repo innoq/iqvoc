a41c555460e153910ebcbcda376a8597b95b6449
Robert Glaser <mail@robert-glaser.de>
2013-10-25 15:00:42 +0200

    implement early draft of alphabetical api adaptor
    still not sure about the locale thing /cc @FND

---
+++ app/search_adaptors/iqvoc_alphabetical_search_adaptor.rb
@@ -0,0 +1,36 @@
+require 'faraday'
+require 'nokogiri'
+require 'linkeddata'
+
+class IqvocAlphabeticalSearchAdaptor < SearchAdaptor
+  def search(locale, prefix)
+    fetch_results("/#{locale}/alphabetical_concepts/#{prefix}.html")
+    @results
+  end
+
+  def fetch_results(url, params = {})
+    begin
+      response = @conn.get(url, params)
+      @results ||= []
+      @results += extract_results(response.body)
+      while more = @doc.at_css('a[rel=next]')
+        fetch_results(more[:href], {})
+      end
+    rescue Faraday::Error::ConnectionFailed,
+      Faraday::Error::ResourceNotFound,
+      Faraday::Error::TimeoutError => e
+        Rails.logger.warn("HTTP error while querying remote source #{url}: #{e.message}")
+        return nil
+    end
+  end
+
+  def extract_results(html)
+    @doc = Nokogiri::HTML(html)
+
+    @doc.css('.concept-item').map do |element|
+      link = element.at_css('.concept-item-link')
+      label, path = link.text, link['data-resource-path']
+      result = SearchResult.new(url, path, label)
+    end
+  end
+end
