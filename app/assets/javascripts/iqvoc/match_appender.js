bfd5f70cc3415ec12a356f9e8d441b70c113a7da
Robert Glaser <mail@robert-glaser.de>
2013-11-12 18:11:05 +0100

    implement autocomplete for concept mapping wizard

--- app/assets/javascripts/iqvoc/match_appender.js
+++ app/assets/javascripts/iqvoc/match_appender.js
@@ -8,7 +8,7 @@ IQVOC.matchAppender = (function($) { // TODO: rename to concept mapper
 function MatchAppender(selector) {
   this.root = selector.jquery ? selector : $(selector);
   var remotes = this.root.data("remotes");
-  remotes[null] = "Sonstiges"; // XXX: i18n
+  remotes[""] = "Sonstiges"; // XXX: i18n
   var self = this;
 
   var categories = $.map(this.extractCategories(), function(desc, id) {
@@ -19,10 +19,17 @@ function MatchAppender(selector) {
   var sources = $.map(remotes, function(name, url) {
     return $("<option />").val(url).text(name)[0];
   });
-  var source = $("<select />").append(sources).insertBefore(this.input);
+  this.source = $("<select />").append(sources).insertBefore(this.input);
   $("<button />").text("✓").insertAfter(this.input).
       click($.proxy(this, "onConfirm"));
   this.category = $("<select />").append(categories).insertAfter(this.input);
+
+  this.input.autocomplete({
+    source: $.proxy(this, "onChange"),
+    search: function(ev, ui) {
+      if (self.source.val() === "") { return false; }
+    }
+  });
 }
 MatchAppender.prototype.extractCategories = function() {
   var labels = $("label", this.root);
@@ -36,10 +43,10 @@ MatchAppender.prototype.extractCategories = function() {
   return data;
 };
 MatchAppender.prototype.onConfirm = function(ev) {
-    ev.preventDefault();
+  ev.preventDefault();
   var textAreaName = this.category.val();
 
-  // Work around faulty simple form generated field prefixes
+  // Work around wrongly generated field prefixes by simple_form
   var index = textAreaName.indexOf('_') + 1;
   textAreaName = textAreaName.substr(index);
 
@@ -47,12 +54,35 @@ MatchAppender.prototype.onConfirm = function(ev) {
   var newValue = textArea.val() + "\n" + this.input.val();
 
   textArea.val($.trim(newValue));
+  this.input.val("");
 };
-
-function onSubmit(ev) {
-  console.log("DONE");
-  ev.preventDefault();
-}
+MatchAppender.prototype.onChange = function(req, callback) {
+  // callback([ { label: "feinstaub", value: "http://russenschlampen.de" } ]);
+  var self = this;
+  $.ajax({
+    type: "GET",
+    url: this.root.data("remote-proxy-url"),
+    data: {
+      prefix: encodeURIComponent(req.term), // Internet is scheiße
+      source: this.source.find("option:selected").text(),
+      layout: 0
+    },
+    success: function() {
+      var args = Array.prototype.slice.apply(arguments);
+      args.push(callback);
+      return self.onResults.apply(self, args);
+    }
+  });
+};
+MatchAppender.prototype.onResults = function(html, status, xhr, callback) {
+  // this.spinner.hide();
+  var doc = $("<div />").append(html);
+  var items = $.map(doc.find(".concept-item-link"), function(node, i) {
+    var el = $(node);
+    return { label: el.text(), value: el.data("resource-url") };
+  });
+  callback(items);
+};
 
 return function(selector) {
   return new MatchAppender(selector);
