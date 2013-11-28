57f49f7558674bdb4836dcf1e96bd99f8f3e52eb
Robert Glaser <mail@robert-glaser.de>
2013-11-28 12:19:28 +0100

    add core assets to precompile config

--- lib/iqvoc/environments/production.rb
+++ lib/iqvoc/environments/production.rb
@@ -63,7 +63,7 @@ module Iqvoc::Environments
 
     # Precompile additional assets.
     # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-    # config.assets.precompile += %w( search.js )
+    config.assets.precompile += Iqvoc.core_assets
 
     # Ignore bad email addresses and do not raise email delivery errors.
     # Set this to true and configure the email server for immediate delivery to raise delivery errors.
