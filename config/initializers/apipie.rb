ac056533abbade566b031b25c4e81f2aa6b6f5d1
Robert Glaser <mail@robert-glaser.de>
2013-12-18 16:05:54 +0100

    turn off api-pie validations

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -5,9 +5,10 @@ Apipie.configure do |config|
   config.app_name                = "iQvoc"
   config.api_base_url            = "/"
   config.doc_base_url            = "/apidoc"
-  # were is your API defined?
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/*.rb"
   config.markup                  = Apipie::Markup::Markdown.new
+  config.validate_value          = false
+  config.validate_presence       = false
   config.app_info                = <<-EOF
 iQvoc is a Vocabulary Management System for the Semantic Web.
 EOF
