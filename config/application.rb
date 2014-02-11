40caf835e30f9aaec843afe05eff30a68599db5e
Robert Glaser <mail@robert-glaser.de>
2014-02-11 16:08:06 +0100

    get rid of i18n deprecation warning

--- config/application.rb
+++ config/application.rb
@@ -51,6 +51,8 @@ module Iqvoc
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
 
+    config.enforce_available_locales = true
+
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
