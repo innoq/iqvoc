2fcb03bf4c5b107d33cd98a99ba0221232177019
Robert Glaser <mail@robert-glaser.de>
2014-04-25 14:26:46 +0200

    fix asset precompile config for rails 4.1

--- lib/iqvoc/environments/production.rb
+++ lib/iqvoc/environments/production.rb
@@ -61,10 +61,6 @@ module Iqvoc::Environments
     # Enable serving of images, stylesheets, and JavaScripts from an asset server.
     # config.action_controller.asset_host = "http://assets.example.com"
 
-    # Precompile additional assets.
-    # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-    config.assets.precompile += Iqvoc.core_assets
-
     # Ignore bad email addresses and do not raise email delivery errors.
     # Set this to true and configure the email server for immediate delivery to raise delivery errors.
     # config.action_mailer.raise_delivery_errors = false
