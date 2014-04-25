80f3f65f03934351415d58430f7ce65ce343858f
Marc Jansing <marc.jansing@innoq.com>
2014-04-25 15:59:23 +0200

    added create export job integration test

---
+++ test/integration/export_test.rb
@@ -0,0 +1,50 @@
+# encoding: UTF-8
+
+# Copyright 2011-2013 innoQ Deutschland GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require File.join(File.expand_path(File.dirname(__FILE__)), '../integration_test_helper')
+
+class ExportTest < ActionDispatch::IntegrationTest
+
+
+  test 'export privileges' do
+    # guest
+    visit exports_path(:lang => 'en')
+    assert page.has_content? 'No permission'
+
+    ['reader', 'editor', 'publisher'].each do |role|
+      login role
+      visit exports_path(:lang => 'en')
+      assert page.has_content?('No permission'), "#{role} must not access exports"
+      logout
+    end
+
+    login 'administrator'
+    visit exports_path(:lang => 'en')
+    assert page.has_content? 'Export'
+  end
+
+  test 'full export generation' do
+    login('administrator')
+
+    visit exports_path(:lang => 'en')
+    assert page.has_content? 'Export'
+
+    select('RDF/N-Triples', :from => 'Type')
+    click_link_or_button 'Request Export'
+    assert page.has_content? 'Export job was created. Reload page to see current processing status.'
+  end
+
+end
