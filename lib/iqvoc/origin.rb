afaeb1cc169411c8f3b4e6c03e99284290cca24a
Robert Glaser <mail@robert-glaser.de>
2014-05-05 17:23:09 +0200

    hashes are ordered by default since Ruby 1.9

--- lib/iqvoc/origin.rb
+++ lib/iqvoc/origin.rb
@@ -60,7 +60,7 @@ module Iqvoc
           run(obj, str)
         end
       end
-      @filters = ActiveSupport::OrderedHash.new
+      @filters = {}
       @filters[:uri_conformance_filter] = UriConformanceFilter
 
       def self.register(name, klass)
