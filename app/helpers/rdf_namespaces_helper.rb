6c92e013a64424724cf65333e496cf08da2ddda9
Robert Glaser <mail@robert-glaser.de>
2014-05-27 15:12:24 +0200

    remove legacy bugfix

--- app/helpers/rdf_namespaces_helper.rb
+++ app/helpers/rdf_namespaces_helper.rb
@@ -3,7 +3,7 @@ module RdfNamespacesHelper
 
   def iqvoc_default_rdf_namespaces
     Iqvoc.rdf_namespaces.merge({
-      default: root_url(format: nil, lang: nil, trailing_slash: true).gsub(/\/\/$/, "/"), # gsub because of a Rails bug :-(
+      default: root_url(format: nil, lang: nil, trailing_slash: true),
       coll: rdf_collections_url(trailing_slash: true, lang: nil, format: nil),
       schema: schema_url(format: nil, anchor: "", lang: nil)
     })
