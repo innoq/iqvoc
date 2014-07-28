7852df513468fb88f456872845994d4592bf1f25
christophw <christoph.wiemers@innoq.com>
2014-07-28 17:06:00 +0200

    adding some tests

--- test/controllers/reverse_match_test.rb
+++ test/controllers/reverse_match_test.rb
@@ -21,12 +21,30 @@ class ReverseMatchTest < ActionController::TestCase
   setup do
     @controller = ConceptsController.new
 
+    @admin = User.create! do |u|
+      u.forename = 'Test'
+      u.surname = 'User'
+      u.email = 'admin@iqvoc'
+      u.password = 'omgomgomg'
+      u.password_confirmation = 'omgomgomg'
+      u.role = 'administrator'
+      u.active = true
+    end
+
     Iqvoc.config.register_setting('sources.iqvoc', ['http://umthes.innoq.com'])
 
     @achievement_hobbies = Concept::SKOS::Base.new.tap do |c|
       Iqvoc::RDFAPI.devour c, 'skos:prefLabel', '"Achievement hobbies"@en'
       c.publish
       c.save
+    end
+
+    @airsoft = Concept::SKOS::Base.new.tap do |c|
+      Iqvoc::RDFAPI.devour c, 'skos:prefLabel', '"Airsoft"@en'
+      c.publish
+      c.save
+      new_version = c.branch @admin
+      new_version.save
     end
 
     @request.env['HTTP_REFERER'] = 'http://iqvoc.net'
@@ -39,5 +57,23 @@ class ReverseMatchTest < ActionController::TestCase
           match_class: 'Match::SKOS::BroadMatch',
           uri: 'http://google.de'
     assert_response 200
+  end
+
+  test 'unknown match class' do
+    patch :add_match,
+          lang: 'en',
+          origin: @achievement_hobbies.origin,
+          match_class: 'UnknownMatch::SKOS::BroadMatch',
+          uri: 'http://google.de'
+    assert_response 400
+  end
+
+  test 'concept locked' do
+    patch :add_match,
+          lang: 'en',
+          origin: @airsoft.origin,
+          match_class: 'Match::SKOS::BroadMatch',
+          uri: 'http://iqvoc.net'
+    assert_response 423
   end
 end
