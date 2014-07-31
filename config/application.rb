2d4818a709519d839e44c6daa5cff04aa11af2b3
christophw <christoph.wiemers@innoq.com>
2014-07-31 08:28:53 +0200

    moving user models in subfolder and adding autoload paths to config

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,6 @@ module Iqvoc
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.autoload_paths += Dir[Rails.root.join('app', 'models', 'user', '{**/}')]
   end
 end
