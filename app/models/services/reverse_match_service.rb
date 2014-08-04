5d1fd26d8b72235b0b266080412619d709663f31
christophw <christoph.wiemers@innoq.com>
2014-08-04 12:14:21 +0200

    make use of url methods and host/port

--- app/models/services/reverse_match_service.rb
+++ app/models/services/reverse_match_service.rb
@@ -1,38 +1,39 @@
-class ReverseMatchService
-  include Rails.application.url_helpers
+module Services
+  class ReverseMatchService
+    include Rails.application.routes.url_helpers
+    #include Iqvoc::Application.routes.url_helpers
+    def initialize(host = nil, port = nil)
+      @host = host
+      @port = port
+    end 
 
-  def initialize(host = nil, port = nil)
-    @host = host
-    @port = port
-  end
+    def add(origin, target_url, match_class)
+      # TODO: Error Handling
+      conn = connection(target_url, { accept: 'application/json' })
+      response = conn.get
+      link = response.body['links'].detect { |h| h['rel'] == 'add_match' }
+      url = link['href']
+      method = link['method']
 
-  def add(origin, target_url, match_class)
-    # TODO: Error Handling
-    conn = connection(target_url, { accept: 'application/json' })
-    response = conn.get
-
-    link = response.body['links'].detect { |h| h['rel'] == 'add_match' }
-    url = link['href']
-    method = link['method']
-
-    # TODO: Error Handling
-    conn = connection(url, { content_type: 'application/json', referer: 'http://0.0.0.0:3000' })
-    response = conn.send(method) do |req|
-      req.params['match_class'] = match_class
-      req.params['uri'] = 'http://0.0.0.0:3000/en/concepts/air_sports.html'
+      # TODO: Error Handling
+      conn = connection(url, { content_type: 'application/json', referer: root_url(host: @host, port: @port) })
+      response = conn.send(method) do |req|
+        req.params['match_class'] = match_class
+        req.params['uri'] = rdf_url(origin, host: @host, port: @port, format: :json) 
+      end
     end
-  end
 
-  def remove(origin, target_url, match_class)
-  end
+    def remove(origin, target_url, match_class)
+    end
 
-  private
+    private
 
-  def connection(url, headers = {})
-    Faraday::Connection.new(url: url, headers: headers) do |builder|
-      builder.use FaradayMiddleware::ParseJson
-      builder.use FaradayMiddleware::FollowRedirects, limit: 5
-      builder.adapter Faraday.default_adapter
+    def connection(url, headers = {})
+      Faraday::Connection.new(url: url, headers: headers) do |builder|
+        builder.use FaradayMiddleware::ParseJson
+        builder.use FaradayMiddleware::FollowRedirects, limit: 5
+        builder.adapter Faraday.default_adapter
+      end
     end
   end
 end
