11e16aa0a908018972955130fd53f412b063e32e
Marc Jansing <marc.jansing@innoq.com>
2014-08-04 09:19:28 +0200

    use http method from info request

--- app/services/ReverseMatchService.rb
+++ app/services/ReverseMatchService.rb
@@ -1,21 +1,23 @@
 class ReverseMatchService
   include Rails.application.url_helpers
-  def initialize(host: nil)
-   @host = host
+
+  def initialize(host = nil, port = nil)
+    @host = host
+    @port = port
   end
-  
+
   def add(origin, target_url, match_class)
     # TODO: Error Handling
     conn = connection(target_url, { accept: 'application/json' })
     response = conn.get
 
-    link = response.body['links'].detect {|h| h['rel'] == 'add_match' }
+    link = response.body['links'].detect { |h| h['rel'] == 'add_match' }
     url = link['href']
     method = link['method']
 
     # TODO: Error Handling
-    conn = connection(url, {content_type: 'application/json', referer: 'http://0.0.0.0:3000'})
-    response = conn.patch do |req|
+    conn = connection(url, { content_type: 'application/json', referer: 'http://0.0.0.0:3000' })
+    response = conn.send(method) do |req|
       req.params['match_class'] = match_class
       req.params['uri'] = 'http://0.0.0.0:3000/en/concepts/air_sports.html'
     end
@@ -26,10 +28,10 @@ class ReverseMatchService
 
   private
 
-  def connection(url, headers={})
-    Faraday::Connection.new( url: url, headers: headers) do |builder|
+  def connection(url, headers = {})
+    Faraday::Connection.new(url: url, headers: headers) do |builder|
       builder.use FaradayMiddleware::ParseJson
-      builder.use FaradayMiddleware::FollowRedirects, limit:5
+      builder.use FaradayMiddleware::FollowRedirects, limit: 5
       builder.adapter Faraday.default_adapter
     end
   end
