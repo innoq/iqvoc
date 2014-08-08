59875ec3cee17cfc8d64d0e2aa4d2bd580a0cacf
Marc Jansing <marc.jansing@innoq.com>
2014-08-08 08:50:18 +0200

    fix trailing whitespaces

--- app/jobs/reverse_match_job.rb
+++ app/jobs/reverse_match_job.rb
@@ -2,7 +2,7 @@ class ReverseMatchJob < Struct.new(:type, :match_class, :subject, :object, :refe
   def enqueue(job)
     JobRelation.create(owner_reference: self.origin, job: job)
   end
-  
+
   def perform
     # TODO: Error Handling
     conn = connection(subject, { accept: 'application/json' })
@@ -10,7 +10,7 @@ class ReverseMatchJob < Struct.new(:type, :match_class, :subject, :object, :refe
     link = response.body['links'].detect { |h| h['rel'] == type.to_s }
     request_url = link['href']
     request_method = link['method']
-    
+
     # TODO: Error Handling
     conn = connection(request_url, { content_type: 'application/json', referer: referer })
     response = conn.send(request_method) do |req|
@@ -18,7 +18,7 @@ class ReverseMatchJob < Struct.new(:type, :match_class, :subject, :object, :refe
       req.params['uri'] = object
     end
   end
-  
+
   def error(job, exception)
     case exception
     when Faraday::ClientError
@@ -31,7 +31,7 @@ class ReverseMatchJob < Struct.new(:type, :match_class, :subject, :object, :refe
       end
     end
   end
- 
+
   def success(job)
     reference = JobRelation.find_by(owner_reference: self.origin, job: job)
     reference.delete
@@ -43,8 +43,8 @@ class ReverseMatchJob < Struct.new(:type, :match_class, :subject, :object, :refe
     Faraday::Connection.new(url: url, headers: headers) do |builder|
       builder.use FaradayMiddleware::ParseJson
       builder.use FaradayMiddleware::FollowRedirects, limit: 5
+      builder.use Faraday::Response::RaiseError
       builder.adapter Faraday.default_adapter
-      builder.use  Faraday::Response::RaiseError
     end
   end
 end
