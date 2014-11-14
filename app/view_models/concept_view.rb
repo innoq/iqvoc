669be23d2d995b4cd5948ee7f11f685939f3cff7
FND <fnd@localhost.localdomain>
2014-11-14 13:56:51 +0100

    introduced language-based categorization, starting with labels
    the assumption here is that most users are interested in one particular
    language (rather than an overview based on the underlying data model)
    
    while the top provides a brief summary (using the selected locale),
    these language sections provide a detailed overview (with non-localized
    values)

--- app/view_models/concept_view.rb
+++ app/view_models/concept_view.rb
@@ -1,5 +1,7 @@
 class ConceptView
-  attr_accessor :title, :definition
+  attr_accessor :title, :definition # TODO: title currently unused
+  attr_accessor :languages # `[{ id, caption }]`
+  attr_accessor :pref_labels, :alt_labels # indexed by language
   attr_accessor :representations
 
   def initialize(concept, ctx) # XXX: `ctx` should not be necessary
@@ -7,6 +9,29 @@ class ConceptView
     @definition = @concept.notes_for_class(Note::SKOS::Definition).first.
         try(:value) # FIXME: hard-coded class, arbitrary pick
 
+    # labels and languages
+    @languages = []
+    @pref_labels = {}
+    @alt_labels = {}
+    Iqvoc::Concept.labeling_classes.each do |labeling_class, languages|
+      (languages || Iqvoc.available_languages).each do |lang| # XXX: `Iqvoc.available_languages` obsolete?
+        @languages << lang # XXX: doesn't belong here, e.g. due to arbitrary order
+
+        collection = labeling_class == Iqvoc::Concept.pref_labeling_class ?
+            @pref_labels : @alt_labels
+        labels = @concept.
+            labels_for_labeling_class_and_language(labeling_class, lang)
+        collection[lang] = labels.map(&:value) if labels.length > 0
+      end
+    end
+    @languages.uniq!.map! do |lang|
+      {
+        'id' => lang,
+        'caption' => ctx.t("languages.#{lang || "-"}")
+      }
+    end
+
+    # resource representations
     published = @concept.published? ? nil : '0'
     @representations = [ # TODO: rename to "links"?
       { 'caption' => 'HTML', 'type' => :link, :format => :html },
