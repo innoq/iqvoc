781dc2e81be8d9d9ba743ac67c503fb8f9da7a16
Robert Glaser <mail@robert-glaser.de>
2014-12-15 13:14:43 +0100

    clean up concept view

--- app/view_models/concept_view.rb
+++ app/view_models/concept_view.rb
@@ -1,9 +1,8 @@
 class ConceptView
-  attr_reader :title # TODO: currently unused
-  attr_reader :languages # `Language`s
-  attr_reader :pref_labels, :alt_labels # strings, indexed by language
+  attr_reader :languages
+  attr_reader :pref_labels, :alt_labels
 
-  class Language # TODO: rename? -- TODO: expose for reuse? -- XXX: un-dry
+  class Language
     attr_reader :id, :caption, :active
 
     def initialize(id, caption, active)
@@ -13,19 +12,19 @@ class ConceptView
     end
   end
 
-  class Link # TODO: expose for reuse? -- XXX: un-dry
+  class Link
     attr_reader :caption, :uri, :type
 
-    def initialize(caption, uri, type=nil)
+    def initialize(caption, uri, type = nil)
       @caption = caption
       @uri = uri
       @type = type
     end
   end
 
-  # `ctx` is the controller context
-  def initialize(concept, ctx) # XXX: `ctx` should not be necessary -- TODO: move complex calculations into separate methods
-    @ctx = ctx
+  # `h` is the controller/helper context which acts as a proxy here
+  def initialize(concept, h)
+    @h = h
     @concept = concept
     @published = @concept.published? ? nil : '0'
   end
@@ -36,8 +35,9 @@ class ConceptView
 
   # returns a string
   def definition
-    @definition ||= @concept.notes_for_class(Note::SKOS::Definition).first. # FIXME: hard-coded class, arbitrary pick
-    try(:value)
+    @definition ||= @concept.notes_for_class(Note::SKOS::Definition)
+      .first # FIXME: hard-coded class, arbitrary pick
+      .try(:value)
   end
 
   def alt_labels
@@ -51,35 +51,20 @@ class ConceptView
     @related = @concept.related_concepts_for_relation_class(klass, @published).
     map do |rel_concept|
       Link.new(rel_concept.pref_label.to_s,
-      @ctx.concept_path(:id => rel_concept))
+      h.concept_path(:id => rel_concept))
     end
   end
 
   # returns a list of `Link`s
   def collections
     @collections ||= @concept.collections.map do |coll|
-      Link.new(coll.label.to_s, @ctx.collection_path(:id => coll))
+      Link.new(coll.label.to_s, h.collection_path(:id => coll))
     end
   end
 
-  # resource representations
-  # returns a list of typed `Link`s
-  def representations
-    @representations ||= [
-      { 'caption' => 'HTML', 'type' => :link, :format => :html },
-      { 'caption' => 'RDF/XML', 'type' => :rdf, :format => :rdf },
-      { 'caption' => 'Turtle', 'type' => :rdf, :format => :ttl },
-      { 'caption' => 'N-Triples', 'type' => :rdf, :format => :nt },
-      { 'caption' => @ctx.t('txt.models.concept.uri'), 'type' => :link,
-        'uri' => @ctx.rdf_url(@concept.origin, :format => nil, :lang => nil,
-        :published => @published) }
-      ].map do |item|
-        unless item['uri'] # assume default URI, keyed on format
-          item['uri'] = @ctx.concept_path(:id => @concept, # XXX: use `rdf_url` w/ `:lang => nil`?
-          :format => item[:format], :published => @published)
-        end
-        Link.new(item['caption'], item['uri'], item['type'])
-      end
-    end
+  private
 
-  end
+  # expose private access to helper proxy in the spirit of the draper gem
+  def h; @h; end
+
+end
