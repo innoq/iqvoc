c7adf89efb8480fd370f522cc29066b5e19ec36a
Marc Jansing <marc.jansing@innoq.com>
2015-03-17 15:55:14 +0100

    store referencing entity for exports and imports

--- app/jobs/export_job.rb
+++ app/jobs/export_job.rb
@@ -1,4 +1,11 @@
 class ExportJob < Struct.new(:export, :filename, :type, :base_uri)
+  def enqueue(job)
+    job.delayed_reference_id   = export.id
+    job.delayed_reference_type = export.class.to_s
+    job.delayed_global_reference_id = export.to_global_id
+    job.save!
+  end
+
   def perform
     strio = StringIO.new
 
