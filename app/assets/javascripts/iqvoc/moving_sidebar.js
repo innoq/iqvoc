f175056006f076ab0a2a221f12ac5f6087d6394a
Marc Jansing <marc.jansing@innoq.com>
2017-07-28 14:05:04 +0200

    recalculate sidebar position

--- app/assets/javascripts/iqvoc/moving_sidebar.js
+++ app/assets/javascripts/iqvoc/moving_sidebar.js
@@ -10,7 +10,26 @@ IQVOC.movingSidebar = (function($) {
 
     // add data-spy="affix" attribute to activate bootstrap's affix
     this.container.attr('data-spy', 'affix');
+    calculateSidebar(container);
+
+    // recalculate on resize
+    $(window).on('resize', IQVOC.debounce(function () {
+      calculateSidebar();
+    }, 250));
   }
+
+  function calculateSidebar(container){
+    var sidebar = $(container);
+
+    if($(document).innerWidth() > 977){
+      var parent = sidebar.parent();
+
+      sidebar.attr('style', '');
+      sidebar.width(parent.width());
+    } else {
+      sidebar.attr('style', 'position:relative;');
+    }
+  };
 
   return function(selector) {
     return new MovingSidebar(selector);
