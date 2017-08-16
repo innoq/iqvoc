6fc0256182238d987a00e050849d46b704efeac4
Marc Jansing <marc.jansing@innoq.com>
2017-08-16 16:12:16 +0200

    Revert "match umlauts in note search"
    This reverts commit 945311962c6c436f71901244a772003316e6fb19.

--- db/migrate/20170816133721_change_notes_value_collation.rb
+++
@@ -1,9 +0,0 @@
-class ChangeNotesValueCollation < ActiveRecord::Migration
-  def up
-    execute("ALTER TABLE notes MODIFY value varchar(4000) CHARACTER SET utf8 COLLATE utf8_bin;")
-  end
-
-  def down
-    execute("ALTER TABLE notes MODIFY value varchar(4000) CHARACTER SET utf8 COLLATE utf8_general_ci;")
-  end
-end
