17556e8b8f234ab7de8f10b5ff001ccb70171696
Marc Jansing <marc.jansing@innoq.com>
2018-05-25 17:03:20 +0200

    fix irreversible migration

--- db/migrate/20180525135715_fix_note_annotations_index.rb
+++ db/migrate/20180525135715_fix_note_annotations_index.rb
@@ -1,6 +1,6 @@
 class FixNoteAnnotationsIndex < ActiveRecord::Migration
   def change
-    remove_index :note_annotations, :value if index_exists? :note_annotations, :value
+    remove_index :note_annotations, column: :value if index_exists? :note_annotations, :value
     add_index :note_annotations, :value
   end
 end
