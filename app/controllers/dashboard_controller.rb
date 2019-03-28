d0a3adbd5b7d00ffe4cdc2c566dd70bb77849469
Philipp Neugebauer <ppn@philippneugebauer.net>
2019-03-28 18:03:50 +0100

    dashboard values are now sorted by labels.value ASC as default

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -31,6 +31,8 @@ class DashboardController < ApplicationController
       order_params = order_params.gsub('value', 'labels.value').gsub('locking_user', 'users.surname').gsub('follow_up', 'concepts.follow_up').gsub('updated_at', 'concepts.updated_at')
 
       concepts = concepts.includes(:pref_labels, :locking_user).references(:pref_labels).references(:locking_user).order(order_params)
+    else
+      concepts = concepts.includes(:pref_labels).order('labels.value')
     end
 
     @items = Kaminari.paginate_array(concepts).page(params[:page])
@@ -52,6 +54,8 @@ class DashboardController < ApplicationController
       order_params = order_params.gsub('value', 'labels.value').gsub('locking_user', 'users.surname').gsub('updated_at', 'concepts.updated_at')
 
       collections = collections.includes(:pref_labels, :locking_user).references(:pref_labels).references(:locking_user).order(order_params)
+    else
+      collections = collections.includes(:pref_labels).order('labels.value')
     end
 
     @items = Kaminari.paginate_array(collections).page(params[:page])
