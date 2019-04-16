c4623d78ae77d9da6603c5aaf7702e90f771b9b0
Philipp Neugebauer <ppn@philippneugebauer.net>
2019-04-16 10:48:29 +0200

    fix tests by checking if mysql version supports new collation

--- db/migrate/20190403133600_change_collation.rb
+++ db/migrate/20190403133600_change_collation.rb
@@ -1,17 +1,26 @@
 class ChangeCollation < ActiveRecord::Migration
+  MYSQL_COLLATION_SUPPORT_VERSION = 8
+
   def up
-    if ActiveRecord::Base.connection.adapter_name == 'Mysql2'
-      change_database_encoding('utf8mb4', 'utf8mb4_0900_as_ci')
-    end
+    change_database_encoding('utf8mb4', 'utf8mb4_0900_as_ci') if check_supporting_mysql_version
   end
 
   def down
-    if ActiveRecord::Base.connection.adapter_name == 'Mysql2'
-      change_database_encoding('utf8', 'utf8mb4_general_ci')
-    end
+    change_database_encoding('utf8', 'utf8mb4_general_ci') if check_supporting_mysql_version
   end
 
   private
+  def check_supporting_mysql_version
+    connection = ActiveRecord::Base.connection
+    if connection.adapter_name == 'Mysql2'
+      version_row = connection.select_rows("SHOW VARIABLES WHERE variable_name = 'version'").try(:first)
+      raise "There is a problem of our code with your MySQL version, please report in GitHub Repository" if version_row.first != "version"
+      version_row.last.first.to_i >= MYSQL_COLLATION_SUPPORT_VERSION
+    else
+      false
+    end
+  end
+
   def change_database_encoding(encoding, collation)
     connection = ActiveRecord::Base.connection
     database = connection.current_database
