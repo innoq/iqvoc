ff752705929b7f3fbd623cfb7254ba733d1c0a58
Marc Jansing <marc.jansing@innoq.com>
2020-05-15 10:39:20 +0200

    remove dedicated job worker logfile

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,4 +1,3 @@
 # keep failed delayed jobs
 Delayed::Worker.destroy_failed_jobs = false
-Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
 Delayed::Worker.max_run_time = 2.days
