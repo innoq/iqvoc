667c50e8d8c4e501d1943a925495f4f3f1f89abb
Marc Jansing <marc.jansing@innoq.com>
2021-01-05 10:14:06 +0100

    remove storage polyfill

--- app/assets/javascripts/framework.js
+++ app/assets/javascripts/framework.js
@@ -4,7 +4,6 @@
 //= require jqtree/tree.jquery.js
 //= require typeahead.js/dist/typeahead.jquery.js
 //= require urijs/src/URI.js
-//= require storage
 //= require popper.js/dist/umd/popper
 //= require bootstrap/dist/js/bootstrap
 //= require bootstrap-datepicker/dist/js/bootstrap-datepicker
