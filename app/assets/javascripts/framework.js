df9c39c66f9c6e9df7b9c9c2360223609812d5c4
Marc Jansing <marc.jansing@innoq.com>
2021-01-19 11:56:58 +0100

    add trailing semicolon

--- app/assets/javascripts/framework.js
+++ app/assets/javascripts/framework.js
@@ -1,5 +1,5 @@
-import 'document-register-element/build/document-register-element.js'
-import 'jquery-ujs/src/rails'
-import 'bootstrap-datepicker/js/bootstrap-datepicker';
-import 'bootstrap-datepicker/js/locales/bootstrap-datepicker.de.js';
-import 'simplete/simplete'
+import 'document-register-element/build/document-register-element.js';
+import 'jquery-ujs/src/rails';
+import 'bootstrap-datepicker/js/bootstrap-datepicker';;
+import 'bootstrap-datepicker/js/locales/bootstrap-datepicker.de.js';;
+import 'simplete/simplete';
