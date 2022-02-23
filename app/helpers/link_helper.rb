46a1286da5a3f01460cc4a0bbc2b05be4059192c
Marc Jansing <marc.jansing@innoq.com>
2022-02-23 10:05:08 +0100

    render full links in search results

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -1,13 +1,13 @@
 module LinkHelper
   def link_to_object(object, name, html_options = nil, &block)
     path = case object
-    when Iqvoc::Concept.base_class
-      concept_path(id: object)
-    when Iqvoc::Collection.base_class
-      collection_path(id: object)
-    when Label::Base
-      label_path(id: object)
-    end
+           when Iqvoc::Concept.base_class
+             concept_url(id: object)
+           when Iqvoc::Collection.base_class
+             collection_url(id: object)
+           when Label::Base
+             label_url(id: object)
+           end
 
     link_to name, path, html_options, &block
   end
